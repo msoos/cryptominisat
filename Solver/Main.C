f63d6be4ce76d7d6ae68f865e2dc2f462588def8
msoos <msoos@6c43cb19-1bce-4953-9960-ac6414807ffa>
2009-10-30 14:15:20 +0000

    Bumping version number, adding NEWS
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cryptominisat/branches/code_cleanup@196 6c43cb19-1bce-4953-9960-ac6414807ffa

--- Solver/Main.C
+++ Solver/Main.C
@@ -467,7 +467,7 @@ int main(int argc, char** argv)
     argc = j;
 
 
-    printf("This is CryptoMiniSat 1.2.10\n");
+    printf("This is CryptoMiniSat 1.2.11\n");
 #if defined(__linux__)
     fpu_control_t oldcw, newcw;
     _FPU_GETCW(oldcw);
