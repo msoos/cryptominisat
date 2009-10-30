976cbe044144c56322db8116623402ff021c359a
msoos <msoos@6c43cb19-1bce-4953-9960-ac6414807ffa>
2009-10-30 11:17:40 +0000

    Added regression tests
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cryptominisat/branches/code_cleanup@176 6c43cb19-1bce-4953-9960-ac6414807ffa

---
+++ tests/regression_test.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import commands
+import os
+import fnmatch
+import gzip
+
+sumt2 = 0.0
+sumprop = 0
+dirList=os.listdir(".")
+for fname in dirList:
+    if fnmatch.fnmatch(fname, '*.cnf.gz'):
+      for i in range(3):
+          of = "outputfile"
+          s2 =  commands.getoutput("../build/cryptominisat -randomize=%d \"%s\" %s"%(i, fname, of))
+          #print s2
+
+          s2 = s2.splitlines()
+          for l in s2:
+              if "CPU time" in l:
+                  t2 = float(l[l.index(":")+1:l.rindex(" s")])
+                  sumt2 += t2
+              if "propagations" in l:
+                  p = int(l[l.index(":")+1:l.rindex("(")])
+                  sumprop += p
+
+
+          print "filename: %20s, exec: %3d, total props: %10d total time:%.2f" %(fname[:20]+"....cnf.gz", i, sumprop, sumt2)
+
+          f = open(of, "r")
+          text = f.read()
+          mylines = text.splitlines()
+          f.close()
+
+          unsat = 0
+          if ('UNSAT' in mylines[0]) :
+              unsat = 1
+          elif ('SAT' in mylines[0]) :
+              unsat = 0
+          else :
+              print "Problem! Maybe didn't finish running? OOOppss!"
+              exit(-1)
+
+          value = {}
+          if (len(mylines) > 1) :
+              vars = mylines[1].split(' ')
+              for var in vars:
+                   vvar = int(var)
+                   if (vvar < 0) :
+                      value[abs(vvar)] = 'false'
+                   else :
+                      value[abs(vvar)] = 'true'
+          
+             
+          #print "FOUND:"
+          #print "unsat: %d" %(unsat)
+          #for k, v in value.iteritems():
+          #    print "var: %d, value: %s" %(k,v)
+          
+          
+          myfname = fname[:len(fname)-6]
+          myfname += "output.gz"
+          f = gzip.open(myfname, "r")
+          text = f.read()
+          f.close()
+          
+          indicated_value = {}
+          indicated_unsat = 0
+          mylines = text.splitlines()
+          for line in mylines :
+            if ('UNSAT' in line) :
+                indicated_unsat = 1
+            elif ('SAT' in line) :
+                indicated_unsat = 0
+            else :
+                #print "line: %s" %(line)
+                stuff = line.split()
+                indicated_value[int(stuff[0])] = stuff[1].lstrip().rstrip()
+        
+          
+          #print "INDICATED:"
+          #print "unsat: %d" %(indicated_unsat)
+          if (unsat != indicated_unsat) :
+              print "UNSAT vs. SAT problem!"
+              exit()
+          else :
+              for k, v in indicated_value.iteritems():
+                  #print "var: %d, value: %s" %(k,v)
+                  if (indicated_value[k] != value[k]) :
+                    print "Problem of found values: values %d: '%s', '%s' don't match!" %(k, value[k], indicated_value[k])
+                    exit()
+
+
+
