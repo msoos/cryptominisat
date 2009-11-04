047cea85cc5b734e99eb9d86f17d30de9ea6ed96
msoos <msoos@6c43cb19-1bce-4953-9960-ac6414807ffa>
2009-11-04 11:26:08 +0000

    Refactoring Main.C: memory and time measurements put into time_mem.h
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cryptominisat/branches/xor_clause_finder@201 6c43cb19-1bce-4953-9960-ac6414807ffa

--- Solver/Main.C
+++ Solver/Main.C
@@ -29,76 +29,11 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #include <zlib.h>
 #include "Logger.h"
 #include "Solver.h"
+#include "time_mem.h"
 using std::cout;
 using std::endl;
 
 /*************************************************************************************/
-#ifdef _MSC_VER
-#include <ctime>
-
-static inline double cpuTime(void)
-{
-    return (double)clock() / CLOCKS_PER_SEC;
-}
-#else
-#ifdef CROSS_COMPILE
-#include <ctime>
-
-static inline double cpuTime(void)
-{
-    return (double)clock() / CLOCKS_PER_SEC;
-}
-#else
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <unistd.h>
-
-static inline double cpuTime(void)
-{
-    struct rusage ru;
-    getrusage(RUSAGE_SELF, &ru);
-    return (double)ru.ru_utime.tv_sec + (double)ru.ru_utime.tv_usec / 1000000;
-}
-#endif
-#endif
-
-
-#if defined(__linux__)
-static inline int memReadStat(int field)
-{
-    char    name[256];
-    pid_t pid = getpid();
-    sprintf(name, "/proc/%d/statm", pid);
-    FILE*   in = fopen(name, "rb");
-    if (in == NULL) return 0;
-    int     value;
-    for (; field >= 0; field--)
-        fscanf(in, "%d", &value);
-    fclose(in);
-    return value;
-}
-static inline uint64_t memUsed()
-{
-    return (uint64_t)memReadStat(0) * (uint64_t)getpagesize();
-}
-
-
-#elif defined(__FreeBSD__)
-static inline uint64_t memUsed(void)
-{
-    struct rusage ru;
-    getrusage(RUSAGE_SELF, &ru);
-    return ru.ru_maxrss*1024;
-}
-
-
-#else
-static inline uint64_t memUsed()
-{
-    return 0;
-}
-#endif
-
 #if defined(__linux__)
 #include <fpu_control.h>
 #endif
