22ebef87d626b0590e0b2eec636bc103152c8af8
msoos <msoos@6c43cb19-1bce-4953-9960-ac6414807ffa>
2009-11-22 20:17:45 +0000

    Much cleaner and much more efficient matrix-finder algorithm
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cryptominisat/branches/gaussian_differentmatrix@412 6c43cb19-1bce-4953-9960-ac6414807ffa

--- Solver/MatrixFinder.cpp
+++ Solver/MatrixFinder.cpp
@@ -21,11 +21,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "Gaussian.h"
 #include "GaussianConfig.h"
 
-#include <list>
 #include <set>
+#include <map>
 #include <iomanip>
-using std::list;
 using std::set;
+using std::map;
+
+//#define VERBOSE_DEBUG
+
+#ifdef VERBOSE_DEBUG
+using std::cout;
+using std::endl;
+#endif
 
 MatrixFinder::MatrixFinder(Solver *_S) :
     S(_S)
@@ -37,86 +44,62 @@ const uint MatrixFinder::findMatrixes()
     if (S->xorclauses.size() == 0)
         return 0;
     
-    typedef list<set<Var> > myset;
-    myset sets;
+    const uint unAssigned = S->nVars() + 1;
+    vector<uint> table(S->nVars(), unAssigned); //var -> matrix
+    map<uint, vector<Var> > reverseTable; //matrix -> vars
+    uint matrix_no = 0;
     
     for (XorClause** c = S->xorclauses.getData(), **end = c + S->xorclauses.size(); c != end; c++) {
-        map<uint, myset::iterator> tomerge;
+        set<uint> tomerge;
+        vector<Var> newSet;
         for (Lit *l = &(**c)[0], *end2 = l + (**c).size(); l != end2; l++) {
-            uint i = 0;
-            for (myset::iterator it = sets.begin(), end3 = sets.end(); it != end3; it++, i++) {
-                set<uint>::iterator finder = it->find(l->var());
-                if (finder != it->end()) {
-                    tomerge[i] = it;
-                    break;
-                }
-            }
+            if (table[l->var()] != unAssigned)
+                tomerge.insert(table[l->var()]);
+            else
+                newSet.push_back(l->var());
         }
-        if (tomerge.empty()) {
-            set<Var> tmp;
-            for (Lit *l = &(**c)[0], *end3 = l + (**c).size(); l != end3; l++)
-                tmp.insert(l->var());
-            sets.push_back(tmp);
-        } else {
-            set<Var>& goodset = *(tomerge.begin()->second);
-            for (map<uint, myset::iterator>::iterator it2 = ++tomerge.begin(), end2 = tomerge.end(); it2 != end2; it2++) {
-                for (set<Var>::iterator it3 = (it2->second)->begin(), end3 = (it2->second)->end(); it3 != end3; it3++) {
-                    goodset.insert(*it3);
-                }
-                sets.erase(it2->second);
-            }
-            for (Lit *l = &(**c)[0], *end2 = l + (**c).size(); l != end2; l++)
-                goodset.insert(l->var());
+        if (newSet.empty() && tomerge.size() == 1)
+            continue;
+        
+        for (set<uint>::iterator it = tomerge.begin(); it != tomerge.end(); it++) {
+            newSet.insert(newSet.end(), reverseTable[*it].begin(), reverseTable[*it].end());
+            reverseTable.erase(*it);
         }
+        for (uint i = 0; i < newSet.size(); i++)
+            table[newSet[i]] = matrix_no;
+        reverseTable[matrix_no] = newSet;
+        matrix_no++;
     }
     
     #ifdef VERBOSE_DEBUG
-    for (myset::iterator it = sets.begin(), end = sets.end(); it != end; it++) {
+    for (map<uint, vector<Var> >::iterator it = reverseTable.begin(), end = reverseTable.end(); it != end; it++) {
         cout << "-- set begin --" << endl;
-        for (set<Var>::iterator it2 = it->begin(), end2 = it->end(); it2 != end2; it2++) {
+        for (vector<Var>::iterator it2 = it->second.begin(), end2 = it->second.end(); it2 != end2; it2++) {
             cout << *it2 << ", ";
         }
         cout << "-------" << endl;
     }
     #endif
     
-    vector<uint> varToSet(S->nVars());
-    vector<uint> setSizes(sets.size());
-    uint matrix_no = 0;
-    for (myset::iterator it = sets.begin(), end = sets.end(); it != end; it++, matrix_no++) {
-        for (set<Var>::iterator it2 = it->begin(), end2 = it->end(); it2 != end2; it2++) {
-            varToSet[*it2] = matrix_no;
-        }
-        setSizes[matrix_no] = it->size();
-    }
-    
     vector<uint> numXorInMatrix(matrix_no, 0);
     for (XorClause** c = S->xorclauses.getData(), **end = c + S->xorclauses.size(); c != end; c++) {
         XorClause& x = **c;
-        numXorInMatrix[varToSet[x[0].var()]]++;
-    }
-    
-    vector<uint> remapMatrixes(matrix_no);
-    for (uint i = 0; i < matrix_no; i++) {
-        remapMatrixes[i] = i;
+        numXorInMatrix[table[x[0].var()]]++;
     }
     
     uint realMatrixNum = 0;
+    vector<uint> remapMatrixes(matrix_no, UINT_MAX);
     for (uint i = 0; i < numXorInMatrix.size(); i++) {
-        if (numXorInMatrix[i] < 20 || numXorInMatrix[i] > 500 || realMatrixNum >= (1 << 12) ) {
-            remapMatrixes[i] = UINT_MAX;
-            for (uint i2 = i+1; i2 < matrix_no; i2++) {
-                remapMatrixes[i2]--;
-            }
-        } else {
-            cout << "|  Matrix no " << std::setw(4) << realMatrixNum << "      no. rows : " << std::setw(5) << numXorInMatrix[i] << " no cols: " << std::setw(6) << setSizes[i] << std::setw(24) << "|" << endl;
+        if (numXorInMatrix[i] >= 20 && numXorInMatrix[i] <= 500 && realMatrixNum < (1 << 12) ) {
+            cout << "|  Matrix no " << std::setw(4) << realMatrixNum << "      no. rows : " << std::setw(5) << numXorInMatrix[i] << " no cols: " << std::setw(6) << reverseTable[i].size() << std::setw(24) << "|" << endl;
+            remapMatrixes[i] = realMatrixNum;
             realMatrixNum++;
         }
     }
     
     for (XorClause** c = S->xorclauses.getData(), **end = c + S->xorclauses.size(); c != end; c++) {
         XorClause& x = **c;
-        const uint toSet = remapMatrixes[varToSet[x[0].var()]];
+        const uint toSet = remapMatrixes[table[x[0].var()]];
         if (toSet != UINT_MAX)
             x.setMatrix(toSet);
         else
