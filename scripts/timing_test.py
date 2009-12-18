958d3b119930b877b4919d3c89028f8f0f37c7cb
msoos <msoos@6c43cb19-1bce-4953-9960-ac6414807ffa>
2009-12-18 13:45:19 +0000

    Making timing_test.py more flexible
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cryptominisat/branches/gaussian_differentmatrix@663 6c43cb19-1bce-4953-9960-ac6414807ffa

--- scripts/timing_test.py
+++ scripts/timing_test.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 import commands
 import os
-# call MiniSat
+import getopt, sys
 
-mydir = "../satfiles/"
-problem = "grain"
+problemDir = "../satfiles/"
+problem = ""
 
 def testOneFile(exe, fname) :
   sumtime = 0.0
   sumprop = 0
-  s2 =  commands.getoutput("%s \"%s%s\""%(exe, mydir,fname))
+  s2 =  commands.getoutput("%s \"%s%s\""%(exe, problemDir, fname))
 
   #print s2
   
@@ -25,10 +25,47 @@ def testOneFile(exe, fname) :
   return (sumprop, sumtime);
 
 
-timingFile = open('timings', 'a')
+def usage():
+  print "--problem (-p)     Name of the problem. E.g. grain, bivium"
+  print "--dir     (-d)     Directory it is found in (default: ../satfiles/)"
+  print "--result  (-r)     Filename where the results will be stored"
+  print "--help    (-h)     Print this help screen"
 
-for i in range (110, 0, -2):
-  dirList=os.listdir(mydir)
+resultFile = "results"
+
+try:
+  opts, args = getopt.getopt(sys.argv[1:], "p:d:r:", ["help", "problem=", "directory=", "result="])
+except getopt.GetoptError, err:
+  print str(err)
+  usage()
+  sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ("-h", "--help"):
+        usage()
+        sys.exit()
+    elif opt in ("-p", "--problem"):
+        problem = arg
+    elif opt in ("-d", "--directory"):
+        problemDir = arg
+    elif opt in ("-r", "--result"):
+        resultFile = arg
+    else:
+        assert False, "unhandled option"
+
+if (problem == "") :
+  print "Problem name must be given!"
+  #usage()
+  sys.exit()
+
+print "Problem name  : %s" %(problem)
+print "Directory name: %s" %(problemDir)
+print "Result file   : %s" %(resultFile)
+
+timingFile = open(resultFile, 'a')
+
+for i in range (60, 0, -2):
+  dirList=os.listdir(problemDir)
   bigSumTime1 = 0.0
   bigSumProp1 = 0
   bigSumTime2 = 0.0
@@ -49,11 +86,7 @@ for i in range (110, 0, -2):
   if (execnum == 0) :
     continue
   print "given help bits: %d, ext1, total props: %10d total time:%.2f" %(i, bigSumProp1, bigSumTime1)
-  timingFile.write("gauss\t%s\t%d\t%d\t%.2f\n" %(problem, execnum, bigSumProp1, bigSumTime1));
+  timingFile.write("gauss\t%s\t%d\t%d\t%d\t%.2f\n" %(problem, execnum, i, bigSumProp1, bigSumTime1));
   print "given help bits: %d, ext2, total props: %10d total time:%.2f" %(i, bigSumProp2, bigSumTime2)
-  timingFile.write("nogauss\t%s\t%d\t%d\t%.2f\n" %(problem, execnum, bigSumProp2, bigSumTime2));
-
-    
-
-
+  timingFile.write("nogauss\t%s\t%d\t%d\t%d\t%.2f\n" %(problem, execnum, i, bigSumProp2, bigSumTime2));
 
