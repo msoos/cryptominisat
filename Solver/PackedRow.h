3ce0e392be840debf17ad09698353d50c9995eea
msoos <msoos@6c43cb19-1bce-4953-9960-ac6414807ffa>
2010-01-06 15:19:31 +0000

    Don't need sys/types.h in PackedRow.h
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cryptominisat/branches/gaussian_differentmatrix@756 6c43cb19-1bce-4953-9960-ac6414807ffa

--- Solver/PackedRow.h
+++ Solver/PackedRow.h
@@ -30,7 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "SolverTypes.h"
 #include "Vec.h"
 #include <string.h>
-#include <sys/types.h>
 #include <iostream>
 #include <algorithm>
 #include <limits>
