6952ac21933d833f5a8af34f7cca1c54d99fbe22
msoos <msoos@6c43cb19-1bce-4953-9960-ac6414807ffa>
2010-01-15 12:03:00 +0000

    Adding a script that helps to do debugging when VERBOSE_DEBUG is turned on
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cryptominisat/branches/gaussian_differentmatrix@767 6c43cb19-1bce-4953-9960-ac6414807ffa

---
+++ scripts/debugscript.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+$var1 = 59;
+$var2 = 61;
+$vars = "($var1|$var2)";
+$group = 645454;
+$valami = '(((var)|(lit)) \b-?'.$vars.'\b)|(Replacing.*\b'.$vars.'\b)|(Statistics)|(Extending.*\b'.$vars.':)|( \| )|(after replacing.*\b-?'.$vars.'\b)|(after)|(after.*'.$vars.')|(Final.*\b-?'.$vars.'\b)|(group: '.$group.')(doCalc.*\b-?'.$vars.'\b)';
+$myexec= 'grep -E "'.$valami.'" output';
+exec $myexec
