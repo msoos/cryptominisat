edb51f9fffe413ea040845583d9970a2d78ee389
msoos <msoos@6c43cb19-1bce-4953-9960-ac6414807ffa>
2010-02-24 16:15:41 +0000

    failed var searcher was wrong in simplifyProblem. At the same place, heule should not be searched
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/cryptominisat/branches/gaussian_differentmatrix@942 6c43cb19-1bce-4953-9960-ac6414807ffa

--- Solver/Solver.C
+++ Solver/Solver.C
@@ -1704,11 +1704,10 @@ const lbool Solver::simplifyProblem(const uint32_t numConfls, const uint64_t num
     if (status != l_Undef)
         goto end;
     
-    if (failedVarSearch && !failedVarSearcher->search(numProps))
+    if (failedVarSearch && !failedVarSearcher->search(numProps)) {
+        status = l_False;
         goto end;
-    
-    /*if (heuleProcess && xorclauses.size() > 1 && !conglomerate->heuleProcessFull())
-        goto end;*/
+    }
     
 end:
     random_var_freq = backup_random_var_freq;
