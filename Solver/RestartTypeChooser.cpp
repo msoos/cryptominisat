a3d17e8338f490c530297aad8cd10ea3b16b2f9c
Mate Soos <mate.soos@inrialpes.fr>
2010-03-19 16:10:27 +0100

    Updating restart type chooser not to choose static restart when the standard deviation of variable degree is too high -- in this case, the problem is most certainly non-cryptographic, and it's best not to do dynamic restarts

--- Solver/RestartTypeChooser.cpp
+++ Solver/RestartTypeChooser.cpp
@@ -21,14 +21,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define VERBOSE_DEBUG
 //#define PRINT_VARS
 
-RestartTypeChooser::RestartTypeChooser(const Solver* const _s) :
-    S(_s)
+RestartTypeChooser::RestartTypeChooser(const Solver& s) :
+    solver(s)
     , topX(100)
     , limit(40)
 {
 }
 
-const RestartType RestartTypeChooser::choose()
+void RestartTypeChooser::addInfo()
 {
     firstVarsOld = firstVars;
     calcHeap();
@@ -43,14 +43,17 @@ const RestartType RestartTypeChooser::choose()
         std::cout << "    Same vars in first&second first 100: " << sameIn << std::endl;
         #endif
         sameIns.push_back(sameIn);
-    } else
-        return static_restart;
+    }
     
     #ifdef VERBOSE_DEBUG
-    std::cout << "Avg same vars in first&second first 100: " << avg() << " standard Deviation:" << stdDeviation() <<std::endl;
+    std::cout << "Avg same vars in first&second first 100: " << avg() << " standard Deviation:" << stdDeviation(sameIns) <<std::endl;
     #endif
-    
-    if (avg() > (double)limit || (avg() > (double)(limit*0.9) && stdDeviation() < 5))
+}
+
+const RestartType RestartTypeChooser::choose()
+{
+    if (countVarsDegreeStDev().second  < 80 &&
+        (avg() > (double)limit || ((avg() > (double)(limit*0.9) && stdDeviation(sameIns) < 5))))
         return static_restart;
     else
         return dynamic_restart;
@@ -64,13 +67,13 @@ const double RestartTypeChooser::avg() const
     return (sum/(double)sameIns.size());
 }
 
-const double RestartTypeChooser::stdDeviation() const
+const double RestartTypeChooser::stdDeviation(vector<uint32_t>& measure) const
 {
     double average = avg();
     double variance = 0.0;
-    for (uint i = 0; i != sameIns.size(); i++)
-        variance += pow((double)sameIns[i]-average, 2);
-    variance /= (double)sameIns.size();
+    for (uint i = 0; i != measure.size(); i++)
+        variance += pow((double)measure[i]-average, 2);
+    variance /= (double)measure.size();
     
     return sqrt(variance);
 }
@@ -82,7 +85,7 @@ void RestartTypeChooser::calcHeap()
     #ifdef PRINT_VARS
     std::cout << "First vars:" << std::endl;
     #endif
-    Heap<Solver::VarOrderLt> tmp(S->order_heap);
+    Heap<Solver::VarOrderLt> tmp(solver.order_heap);
     uint32_t thisTopX = std::min(tmp.size(), topX);
     for (uint32_t i = 0; i != thisTopX; i++) {
         #ifdef PRINT_VARS
@@ -94,3 +97,38 @@ void RestartTypeChooser::calcHeap()
     std::cout << std::endl;
     #endif
 }
+
+const std::pair<double, double> RestartTypeChooser::countVarsDegreeStDev() const
+{
+    vector<uint32_t> degrees;
+    degrees.resize(solver.nVars(), 0);
+    addDegrees(solver.clauses, degrees);
+    addDegrees(solver.binaryClauses, degrees);
+    addDegrees(solver.xorclauses, degrees);
+    uint32_t sum = 0;
+    for (uint i = 0; i < degrees.size(); i++)
+        sum += degrees[i];
+    double avg = (double)sum/(double)degrees.size();
+    double stdDev = stdDeviation(degrees);
+    
+    //#ifdef VERBOSE_DEBUG
+    std::cout << "varsDegree avg:" << avg << " stdDev:" << stdDev << std::endl;
+    //#endif
+    
+    return std::make_pair(avg, stdDev);
+}
+
+template<class T>
+void RestartTypeChooser::addDegrees(const vec<T*>& cs, vector<uint32_t>& degrees) const
+{
+    for (T * const*c = cs.getData(), * const*end = c + cs.size(); c != end; c++) {
+        T& cl = **c;
+        for (const Lit *l = cl.getData(), *end2 = l + cl.size(); l != end2; l++) {
+            degrees[l->var()]++;
+        }
+    }
+}
+
+template void RestartTypeChooser::addDegrees(const vec<Clause*>& cs, vector<uint32_t>& degrees) const;
+template void RestartTypeChooser::addDegrees(const vec<XorClause*>& cs, vector<uint32_t>& degrees) const;
+
