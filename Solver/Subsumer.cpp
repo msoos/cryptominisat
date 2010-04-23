6194f1035c20441479c049856d89fcb38b5a6463
Mate Soos <mate.soos@inrialpes.fr>
2010-04-23 19:18:56 +0200

    Somewhat cleaner Subsumer's varelim var ordering
    touched_list[i] was 'var' anyway, so re-use that

--- Solver/Subsumer.cpp
+++ Solver/Subsumer.cpp
@@ -943,7 +943,7 @@ const bool Subsumer::simplifyBySubsumption()
                     const Var var = touched_list[i];
                     if (!var_elimed[var] && !cannot_eliminate[var] && solver.decision_var[var] && solver.assigns[var] == l_Undef) {
                         order.push(touched_list[i]);
-                        touched[touched_list[i]] = 0;
+                        touched[var] = 0;
                     }
                 }
                 touched_list.clear();
