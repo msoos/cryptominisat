7c9f2c5b187249aaf0ca0629497a5e7eb01edd91
Mate Soos <mate.soos@inrialpes.fr>
2010-07-04 19:53:22 +0200

    Adding assert and cleaning up ClauseCleaner
    Size of clause must be > 1 and size==3 must not be treated specially

--- Solver/ClauseCleaner.cpp
+++ Solver/ClauseCleaner.cpp
@@ -137,6 +137,7 @@ inline const bool ClauseCleaner::cleanClause(Clause*& cc)
     }
     c.shrink(i-j);
 
+    assert(c.size() != 1);
     if (i != j) {
         c.setStrenghtened();
         if (c.size() == 2) {
@@ -145,12 +146,6 @@ inline const bool ClauseCleaner::cleanClause(Clause*& cc)
             solver.clauseAllocator.clauseFree(&c);
             cc = c2;
             solver.attachClause(*c2);
-        /*} else if (c.size() == 3) {
-            solver.detachModifiedClause(origLit1, origLit2, origSize, &c);
-            Clause *c2 = Clause_new(c);
-            clauseFree(&c);
-            cc = c2;
-            solver.attachClause(*c2);*/
         } else {
             if (c.learnt())
                 solver.learnts_literals -= i-j;
