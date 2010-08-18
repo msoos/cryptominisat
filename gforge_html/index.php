d3af4a2d843d498b90621fc470bb1dcf9360bc59
Mate Soos <mate.soos@inrialpes.fr>
2010-08-18 08:54:20 +0200

    Adding GForge HTML and upload script
    Standard procedure to upload file to the CryptoMiniSat GForge homepage

---
+++ gforge_html/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="1; URL=http://www.msoos.org/wordpress/?page_id=99">
+</head>
+<body>
+<p align="center">The page you are looking for is not here. Try:</p>
+<p align="center">
+<a href="http://www.msoos.org/wordpress/?page_id=99">
+http://www.msoos.org/wordpress/?page_id=99</a></p>
+<p align="center">If you are not redirected automatically within a few
+seconds then please click on the link above.</p>
+</body>
+</html>
