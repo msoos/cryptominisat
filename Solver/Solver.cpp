5b64d9e6d18fbef61e2820b07de1084a3ec6f4ea
Mate Soos <mate.soos@inrialpes.fr>
2010-11-16 01:05:38 +0100

    It's too slow to perform failed lit search at startup

--- Solver/Solver.cpp
+++ Solver/Solver.cpp
@@ -2403,10 +2403,10 @@ void Solver::performStepsBeforeSolve()
     if (conf.doClausVivif && !conf.libraryUsage
         && !clauseVivifier->vivifyClauses()) return;
 
-    bool saveDoHyperBin = conf.doHyperBinRes;
+    /*bool saveDoHyperBin = conf.doHyperBinRes;
     conf.doHyperBinRes = false;
     if (conf.doFailedLit && !failedLitSearcher->search()) return;
-    conf.doHyperBinRes = saveDoHyperBin;
+    conf.doHyperBinRes = saveDoHyperBin;*/
 
     if (conf.doSatELite
         && !conf.libraryUsage
