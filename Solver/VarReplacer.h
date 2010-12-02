ef325a8c8b7d6df7d38100673b4b7e5c29cc5e7a
Mate Soos <mate.soos@inrialpes.fr>
2010-12-03 00:01:40 +0100

    Updating header of VarReplacer.

--- Solver/VarReplacer.h
+++ Solver/VarReplacer.h
@@ -1,4 +1,4 @@
-/***********************************************************************************
+/***************************************************************************
 CryptoMiniSat -- Copyright (c) 2009 Mate Soos
 
 This program is free software: you can redistribute it and/or modify
@@ -13,7 +13,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-**************************************************************************************************/
+*****************************************************************************/
 
 #ifndef VARREPLACER_H
 #define VARREPLACER_H
