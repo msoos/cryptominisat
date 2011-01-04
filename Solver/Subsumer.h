0a0a4cfb79a25f5f8914161088b917885acdc5e8
Mate Soos <mate.soos@inrialpes.fr>
2011-01-04 20:31:56 +0100

    Much faster AND gate-based clause removability detection
    We sort the clauses in the occur lists into sizes, and then go through
    them that way. For occur sizes that sometimes are in the wild > 800 for
    example, this technique is MUCH faster

--- Solver/Subsumer.h
+++ Solver/Subsumer.h
@@ -355,7 +355,8 @@ private:
     const bool treatAndGates();
     const bool treatAndGate(const OrGate& gate, const bool reallyRemove, uint32_t& foundPotential);
     const bool treatAndGateClause(vec<ClauseSimp>& others, std::set<uint32_t>& clToUnlink, const OrGate& gate, const Clause& cl, const uint32_t clIndex);
-    void findAndGateOtherCl(const Lit lit, const uint32_t size, const uint32_t abst2, vec<ClauseSimp>& others);
+    void findAndGateOtherCl(const vector<ClauseSimp>& sizeSortedOcc, const Lit lit, const uint32_t abst2, vec<ClauseSimp>& others);
+    vector<vector<ClauseSimp> > sizeSortedOcc;
 
     //validity checking
     const bool verifyIntegrity();
