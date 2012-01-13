66cd45251a45e12e17638120f6616577837de2e6
Mate Soos <soos.mate@gmail.com>
2012-01-13 18:33:18 +0100

    Check if the problem is solved (unsat AND sat) after search()

--- Solver/CommandControl.cpp
+++ Solver/CommandControl.cpp
@@ -1213,7 +1213,7 @@ lbool CommandControl::solve(const vector<Lit>& assumps, const uint64_t maxConfls
         assert(numConflicts < maxConfls);
         status = search(SearchFuncParams(maxConfls-numConflicts), rest);
         rest *= conf.restart_inc;
-        if (status == l_False)
+        if (status != l_Undef)
             break;
 
         if (lastSumConfl >= maxConfls)
