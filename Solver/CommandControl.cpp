45419974c95a2665857c779617b955522dc3f831
Mate Soos <soos.mate@gmail.com>
2012-01-15 19:18:24 +0100

    These functions have been renamed

--- Solver/CommandControl.cpp
+++ Solver/CommandControl.cpp
@@ -299,8 +299,8 @@ void CommandControl::analyze(PropBy confl, vector<Lit>& out_learnt, uint32_t& ou
         && conf.doMinimLearntMore
         && out_learnt.size() > 1
         && (conf.doAlwaysFMinim
-            || calcNBLevels(out_learnt) < 0.65*glueHistory.getAvgAllDouble()
-            || out_learnt.size() < 0.65*conflSizeHist.getAvgAllDouble()
+            || calcNBLevels(out_learnt) < 0.65*glueHist.getAvgAll()
+            || out_learnt.size() < 0.65*conflSizeHist.getAvgAll()
             )
     ) {
         minimiseLearntFurther(out_learnt);
