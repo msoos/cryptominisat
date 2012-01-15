d7d549ac27f0c87e207c90882a865157c96870d2
Mate Soos <soos.mate@gmail.com>
2012-01-15 22:08:31 +0100

    Making 'complete' parametrised

--- utils/complete.cc
+++ utils/complete.cc
@@ -1,18 +1,34 @@
 #include <cstdio>
-#include <cstdint>
-#include <random>
+#include <stdint.h>
+#include "assert.h"
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
 
 int main(int argc, char *argv[])
 {
-	static const unsigned int nr_vars = 15;
+    assert(argc == 2);
+    printf("c argv[1]: %s\n", argv[0]);
+    printf("c argv[1]: %s\n", argv[1]);
 
-	uint32_t x = 0;
-	do {
-		for (unsigned int i = 0; i < nr_vars; ++i)
-			printf("%s%u ", (x >> i) & 1 ? "-" : "", 1 + i);
+    const long int nr_vars = strtol(argv[1], NULL, 10);
 
-		printf("0\n");
-	} while ((++x & ((1U << nr_vars) - 1)) != 0);
+    if ((errno == ERANGE && (nr_vars == LONG_MAX || nr_vars == LONG_MIN))
+           || (errno != 0 && nr_vars == 0)) {
+        perror("strtol");
+        exit(EXIT_FAILURE);
+    }
+    if (nr_vars > 62) {
+        fprintf(stderr, "Too large value given: %d\n", (int)nr_vars);
+    }
 
-	return 0;
+    unsigned long x = 0;
+    do {
+    for (int i = 0; i < nr_vars; ++i)
+        printf("%s%u ", (x >> i) & 1 ? "-" : "", 1 + i);
+
+        printf("0\n");
+    } while ((++x & ((1UL << nr_vars) - 1)) != 0);
+
+    return 0;
 }
