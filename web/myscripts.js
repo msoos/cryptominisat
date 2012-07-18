bfc2f0868aa75d677e25e2f335ea18be62453b0b
Mate Soos <soos.mate@gmail.com>
2012-07-19 00:17:44 +0200

    The column was fixe here only for debug reasons

--- web/myscripts.js
+++ web/myscripts.js
@@ -31,8 +31,7 @@ function drawOneGraph(i)
             underlayCallback: function(canvas, area, g) {
                 canvas.fillStyle = "rgba(105, 105, 185, 185)";
                 //Draw simplification points
-                //colnum = myData[i].colnum;
-                colnum = 0;
+                colnum = myData[i].colnum;
 
                 for(var k = 0; k < simplificationPoints[colnum].length-1; k++) {
                     var bottom_left = g.toDomCoords(simplificationPoints[colnum][k], -20);
