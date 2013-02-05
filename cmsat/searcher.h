e344c41864b790db3c6c279a032962eb4083a233
Mate Soos <soos.mate@gmail.com>
2013-02-05 13:47:07 +0100

    Reset var_inc in case of need

--- cmsat/searcher.h
+++ cmsat/searcher.h
@@ -780,8 +780,16 @@ inline void Searcher::varBumpActivity(Var var)
 
         //If var_inc is smaller than var_inc_start then this MUST be corrected
         //otherwise the 'varDecayActivity' may not decay anything in fact
-        /*if (var_inc < conf.var_inc_start)
-            var_inc = conf.var_inc_start;*/
+        if (var_inc < conf.var_inc_start) {
+            /*cout
+            << "WHAAAAAAAAAAAAAT!!!? var_inc < conf.var_inc_start ! "
+            << var_inc
+            << ", "
+            << conf.var_inc_start
+            << endl;*/
+
+            var_inc = conf.var_inc_start;
+        }
     }
 
     // Update order_heap with respect to new activity:
