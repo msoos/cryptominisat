9dc67e411cebb58683e2645149e55b2005b1df57
Mate Soos <soos.mate@gmail.com>
2013-03-22 21:35:08 +0100

    Use the system in cluster129, and take <<24 as limit instead of <<30 during var bump

--- cmsat/searcher.h
+++ cmsat/searcher.h
@@ -841,8 +841,8 @@ inline void Searcher::varDecayActivity()
 inline void Searcher::varBumpActivity(Var var)
 {
     activities[var] += var_inc;
-    if ( (activities[var]) > ((0x1U) << 30)
-        || var_inc > ((0x1U) << 30)
+    if ( (activities[var]) > ((0x1U) << 24)
+        || var_inc > ((0x1U) << 24)
     ) {
         // Rescale:
         for (vector<uint32_t>::iterator
