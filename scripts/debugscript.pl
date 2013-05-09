ee9ec923a5b17b5519f8c5b5655f7afeff1340a9
Mate Soos <soos.mate@gmail.com>
2013-05-09 21:37:11 +0200

    Removing scripts no longer used

--- scripts/debugscript.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-$var1 = 59;
-$var2 = 61;
-$vars = "($var1|$var2)";
-$group = 645454;
-$valami = '(((var)|(lit)) \b-?'.$vars.'\b)|(Replacing.*\b'.$vars.'\b)|(Statistics)|(Extending.*\b'.$vars.':)|( \| )|(after replacing.*\b-?'.$vars.'\b)|(after)|(after.*'.$vars.')|(Final.*\b-?'.$vars.'\b)|(group: '.$group.')|(doCalc.*\b-?'.$vars.'\b)|(^Var:.*'.$vars.')';
-$myexec= 'grep -E "'.$valami.'" output';
-exec $myexec
