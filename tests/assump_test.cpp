522656c8ea2f645343e1af0349da5a5953711eea
Mate Soos <soos.mate@gmail.com>
2013-09-01 17:33:06 +0200

    Adding minimal unit tests
    Thanks to Lukas Prokop for nudging me along the way :)

---
+++ tests/assump_test.cpp
@@ -0,0 +1,114 @@
+#define BOOST_TEST_MODULE basic_tests
+#include <boost/test/unit_test.hpp>
+
+#include "solver.h"
+using namespace CMSat;
+
+BOOST_AUTO_TEST_SUITE( assumptions_interface )
+
+BOOST_AUTO_TEST_CASE(empty)
+{
+    Solver s;
+    s.newVar();
+    s.addClause(vector<Lit>{Lit(0, false)});
+    vector<Lit> assumps;
+    lbool ret = s.solve(&assumps);
+    BOOST_CHECK_EQUAL( ret, l_True);
+}
+
+BOOST_AUTO_TEST_CASE(single_true)
+{
+    Solver s;
+    s.newVar();
+    s.addClause(vector<Lit>{Lit(0, false)});
+    vector<Lit> assumps;
+    assumps.push_back(Lit(0, false));
+    lbool ret = s.solve(&assumps);
+    BOOST_CHECK_EQUAL( ret, l_True);
+}
+
+BOOST_AUTO_TEST_CASE(single_false)
+{
+    Solver s;
+    s.newVar();
+    s.addClause(vector<Lit>{Lit(0, false)});
+    vector<Lit> assumps;
+    assumps.push_back(Lit(0, true));
+    lbool ret = s.solve(&assumps);
+    BOOST_CHECK_EQUAL( ret, l_False);
+    BOOST_CHECK_EQUAL( s.conflict.size(), 1);
+    BOOST_CHECK_EQUAL( s.conflict[0], Lit(0, false));
+}
+
+BOOST_AUTO_TEST_CASE(binclause_true)
+{
+    Solver s;
+    s.newVar();
+    s.newVar();
+    s.addClause(vector<Lit>{Lit(0, false), Lit(1, false)});
+    vector<Lit> assumps;
+    assumps.push_back(Lit(0, true));
+    lbool ret = s.solve(&assumps);
+    BOOST_CHECK_EQUAL( ret, l_True );
+    BOOST_CHECK_EQUAL( s.model[0], l_False );
+    BOOST_CHECK_EQUAL( s.model[1], l_True );
+}
+
+BOOST_AUTO_TEST_CASE(binclause_false)
+{
+    Solver s;
+    s.newVar();
+    s.newVar();
+    s.addClause(vector<Lit>{Lit(0, false), Lit(1, false)});
+    vector<Lit> assumps;
+    assumps.push_back(Lit(0, true));
+    assumps.push_back(Lit(1, true));
+    lbool ret = s.solve(&assumps);
+    BOOST_CHECK_EQUAL( ret, l_False);
+    BOOST_CHECK_EQUAL( s.conflict.size(), 2);
+    vector<Lit> tmp = s.conflict;
+    std::sort(tmp.begin(), tmp.end());
+    BOOST_CHECK_EQUAL( tmp[0], Lit(0, false) );
+    BOOST_CHECK_EQUAL( tmp[1], Lit(1, false) );
+}
+
+BOOST_AUTO_TEST_CASE(replace_true)
+{
+    Solver s;
+    s.newVar();
+    s.newVar();
+    s.addClause(vector<Lit>{Lit(0, false), Lit(1, true)});
+    s.addClause(vector<Lit>{Lit(0, true), Lit(1, false)});
+    vector<Lit> assumps;
+    assumps.push_back(Lit(0, true));
+    assumps.push_back(Lit(1, true));
+    lbool ret = s.solve(&assumps);
+    BOOST_CHECK_EQUAL( ret, l_True);
+    BOOST_CHECK_EQUAL( s.model[0], l_False );
+    BOOST_CHECK_EQUAL( s.model[1], l_False );
+}
+
+BOOST_AUTO_TEST_CASE(replace_false)
+{
+    Solver s;
+    s.newVar();
+    s.newVar();
+    s.addClause(vector<Lit>{Lit(0, false), Lit(1, true)});
+    s.addClause(vector<Lit>{Lit(0, true), Lit(1, false)});
+
+    vector<Lit> assumps;
+    assumps.push_back(Lit(0, false));
+    assumps.push_back(Lit(1, true));
+
+    lbool ret = s.solve(&assumps);
+    BOOST_CHECK( ret == l_False);
+
+    BOOST_CHECK( s.conflict.size() == 2);
+    vector<Lit> tmp = s.conflict;
+    std::sort(tmp.begin(), tmp.end());
+    BOOST_CHECK( tmp[0] == Lit(0, true) );
+    BOOST_CHECK( tmp[1] == Lit(1, false) );
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ tests/basic_test.cpp
@@ -0,0 +1,67 @@
+#define BOOST_TEST_MODULE basic_tests
+#include <boost/test/unit_test.hpp>
+
+#include "solver.h"
+using namespace CMSat;
+
+BOOST_AUTO_TEST_SUITE( minimal_interface )
+
+BOOST_AUTO_TEST_CASE(start)
+{
+    Solver s;
+    lbool ret = s.solve();
+    BOOST_CHECK_EQUAL( ret, l_True);
+}
+
+BOOST_AUTO_TEST_CASE(onelit)
+{
+    Solver s;
+    s.newVar();
+    s.addClause(vector<Lit>{Lit(0, false)});
+    lbool ret = s.solve();
+    BOOST_CHECK_EQUAL( ret, l_True);
+}
+
+BOOST_AUTO_TEST_CASE(twolit)
+{
+    Solver s;
+    s.newVar();
+    s.addClause(vector<Lit>{Lit(0, false)});
+    s.addClause(vector<Lit>{Lit(0, true)});
+    lbool ret = s.solve();
+    BOOST_CHECK_EQUAL( ret, l_False);
+}
+
+BOOST_AUTO_TEST_CASE(multi_solve_unsat)
+{
+    Solver s;
+    s.newVar();
+    s.addClause(vector<Lit>{Lit(0, false)});
+    s.addClause(vector<Lit>{Lit(0, true)});
+    lbool ret = s.solve();
+    BOOST_CHECK_EQUAL( ret, l_False);
+    for(size_t i = 0;i < 10; i++) {
+        lbool ret = s.solve();
+        BOOST_CHECK_EQUAL( ret, l_False);
+    }
+}
+
+
+/*struct F {
+    F() : i( 1 ) { BOOST_TEST_MESSAGE( "setup fixture" ); }
+    ~F()         { BOOST_TEST_MESSAGE( "teardown fixture" ); }
+
+    int i;
+};
+
+BOOST_FIXTURE_TEST_CASE( test_fix, F)
+{
+    BOOST_CHECK( i == 1 );
+}
+
+BOOST_FIXTURE_TEST_CASE( test_fix2, F)
+{
+    BOOST_CHECK_EQUAL( i, 1 );
+}*/
+
+BOOST_AUTO_TEST_SUITE_END()
