6f9f17d3933667ddbf5beb09bec97e12a5e7f2b0
Mate Soos <soos.mate@gmail.com>
2013-10-14 22:55:21 +0200

    Better and cleaner VarData::stats

--- cmsat/vardata.h
+++ cmsat/vardata.h
@@ -1,36 +1,27 @@
 #ifndef __VARDATA_H__
 #define __VARDATA_H__
 
+#include "propby.h"
+
 namespace CMSat
 {
 using namespace CMSat;
 
 struct VarData
 {
+    #ifdef STATS_NEEDED
     struct Stats
     {
-        Stats() :
-            posPolarSet(0)
-            , negPolarSet(0)
-            #ifdef STATS_NEEDED
-            , posDecided(0)
-            , negDecided(0)
-            , flippedPolarity(0)
-            #endif
-        {}
-
         void addData(VarData::Stats& other)
         {
             posPolarSet += other.posPolarSet;
             negPolarSet += other.negPolarSet;
-            #ifdef STATS_NEEDED
             posDecided += other.posDecided;
             negDecided += other.negDecided;
             flippedPolarity += other.flippedPolarity;
 
             trailLevelHist.addData(other.trailLevelHist);
             decLevelHist.addData(other.decLevelHist);
-            #endif
         }
 
         void reset()
@@ -40,24 +31,23 @@ struct VarData
         }
 
         ///Number of times positive/negative polarity has been set
-        uint32_t posPolarSet;
-        uint32_t negPolarSet;
+        uint32_t posPolarSet = 0;
+        uint32_t negPolarSet = 0;
 
-        #ifdef STATS_NEEDED
         ///Decided on
-        uint32_t posDecided;
-        uint32_t negDecided;
+        uint32_t posDecided = 0;
+        uint32_t negDecided = 0;
 
         ///Number of times polarity has been flipped
-        uint32_t flippedPolarity;
+        uint32_t flippedPolarity = 0;
 
         ///The history of levels it was assigned
         AvgCalc<uint32_t> trailLevelHist;
 
         ///The history of levels it was assigned
         AvgCalc<uint32_t> decLevelHist;
-        #endif
     };
+    #endif
 
     VarData() :
         level(std::numeric_limits< uint32_t >::max())
