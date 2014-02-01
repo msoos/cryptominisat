743ee99af2a665a42fea24c93fc3cd3a109d70b2
Mate Soos <soos.mate@gmail.com>
2014-02-01 20:13:52 +0100

    Increase random polar flipping frequency

--- cryptominisat3/solverconf.cpp
+++ cryptominisat3/solverconf.cpp
@@ -138,7 +138,7 @@ SolverConf::SolverConf() :
         , doLHBR           (false)
         , propBinFirst     (false)
         , dominPickFreq    (400)
-        , polarity_flip_frequency_multiplier(300)
+        , polarity_flip_frequency_multiplier(50)
 
         //Simplifier
         , simplify_at_startup(false)
