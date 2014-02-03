0b86ef1e901d88fb306ba9154cfaa2a5e4757544
Mate Soos <soos.mate@gmail.com>
2014-02-03 20:00:21 +0100

    Executable is now cryptominisat3

--- scripts/todo/todo.sh
+++ scripts/todo/todo.sh
@@ -9,7 +9,7 @@ shopt -s nullglob
 rm -f todo
 touch todo
 fileloc="/home/soos/sat/examples/satcomp091113/*cnf.gz"
-solver="/home/soos/cryptominisat/build/cryptominisat"
+solver="/home/soos/cryptominisat/build/cryptominisat3"
 #solver="/home/soos/lingeling-ala-b02aa1a-121013/lingeling -v"
 #solver="/home/soos/glucose2.2/simp/glucose"
 #opts="--restart glue --clean glue --flippolarfreq 0"
