d43d6200bee2ed3a0d8c8e277cacee52c82790f0
Mate Soos <soos.mate@gmail.com>
2014-06-05 01:40:39 +0200

    Removing commented-out code

--- cryptominisat4/solver.cpp
+++ cryptominisat4/solver.cpp
@@ -1317,7 +1317,6 @@ lbool Solver::solve()
     conflict.clear();
     check_config_parameters();
     if (solveStats.num_solve_calls > 1) {
-        //conf.do_calc_polarity_first_time = false;
         conf.do_calc_polarity_every_time = false;
     }
 
