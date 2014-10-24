5229a8670d0f509f80678b8b12f23fec0b3d79aa
Mate Soos <soos.mate@gmail.com>
2014-10-24 14:58:09 +0200

    Removing superflous word "either" from the license

--- cryptominisat4/cset.h
+++ cryptominisat4/cset.h
@@ -7,7 +7,7 @@
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
+ * License as published by the Free Software Foundation
  * version 2.0 of the License.
  *
  * This library is distributed in the hope that it will be useful,
