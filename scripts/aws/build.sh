d0c9993a6c8c369580ea115e9361eed549aa271d
Mate Soos from AWS <soos.mate@gmail.com>
2014-12-20 22:31:27 +0000

    Adding build script to AWS

---
+++ scripts/aws/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+cd /home/ubuntu/cryptominisat
+rc=$?
+if [[ $rc != 0 ]] ; then
+    exit $rc
+fi
+echo "cd-d into cmsat"
+
+git pull
+rc=$?
+if [[ $rc != 0 ]] ; then
+    exit $rc
+fi
+echo "pulled"
+
+cd build
+rc=$?
+if [[ $rc != 0 ]] ; then
+    exit $rc
+fi
+echo "cd-d into build"
+
+rm -rf C* c*
+rc=$?
+if [[ $rc != 0 ]] ; then
+    exit $rc
+fi
+echo "deleted build"
+
+cmake ..
+rc=$?
+if [[ $rc != 0 ]] ; then
+    exit $rc
+fi
+echo "cmake-d"
+
+make -j2
+rc=$?
+if [[ $rc != 0 ]] ; then
+    exit $rc
+fi
+echo "built"
+
+exit 0
