6a4ba47ca551d7958496e4dbdc1ad83a475c9396
Mate Soos <soos.mate@gmail.com>
2014-12-21 04:59:18 +0100

    Much more robust pre-build script

--- scripts/aws/pre-build.sh
+++ scripts/aws/pre-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/cryptominisat
-git stash
-git checkout -f master
-git pull
+git checkout -f remotes/origin/master
+git branch -D master
+git checkout -b master
+git pull origin master
 /home/ubuntu/cryptominisat/scripts/aws/client.py --host 172.30.0.242 > /home/ubuntu/log.txt &
