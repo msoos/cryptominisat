c36d1339bb153fb7762a9adc45c20168cff0efe7
Mate Soos <soos.mate@gmail.com>
2014-12-21 04:52:32 +0100

    Try to make pre-build more robust

--- scripts/aws/pre-build.sh
+++ scripts/aws/pre-build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/cryptominisat
+git stash
+git checkout -f master
 git pull
 /home/ubuntu/cryptominisat/scripts/aws/client.py --host 172.30.0.242 > /home/ubuntu/log.txt &
