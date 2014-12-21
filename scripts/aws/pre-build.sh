c33c587b227aa925c98f3fd4ef1507ef60c3e414
Mate Soos <soos.mate@gmail.com>
2014-12-21 05:34:54 +0100

    Better git for AWS

--- scripts/aws/pre-build.sh
+++ scripts/aws/pre-build.sh
@@ -4,5 +4,6 @@ git stash
 git checkout -f remotes/origin/master
 git branch -D master
 git checkout -b master
+git branch --set-upstream-to=origin/master master
 git pull origin master
 /home/ubuntu/cryptominisat/scripts/aws/client.py --host 172.30.0.242 > /home/ubuntu/log.txt &
