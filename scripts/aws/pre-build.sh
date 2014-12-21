64b3833bfb4a69220586f9442fb7587e88da67b4
Mate Soos <soos.mate@gmail.com>
2014-12-21 19:26:18 +0100

    Update IP of the server

--- scripts/aws/pre-build.sh
+++ scripts/aws/pre-build.sh
@@ -6,4 +6,4 @@ git branch -D master
 git checkout -b master
 git branch --set-upstream-to=origin/master master
 git pull origin master
-/home/ubuntu/cryptominisat/scripts/aws/client.py --host 172.30.0.242 > /home/ubuntu/log.txt  2>&1 &
+/home/ubuntu/cryptominisat/scripts/aws/client.py --host 172.30.0.208 > /home/ubuntu/log.txt  2>&1 &
