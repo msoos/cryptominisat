5996a4cf4e94866489b83ebe509693915b05a792
Mate Soos <soos.mate@gmail.com>
2014-12-21 05:19:21 +0100

    Don't shut down that easily

--- scripts/aws/client.py
+++ scripts/aws/client.py
@@ -310,7 +310,8 @@ def shutdown() :
     toexec = "sudo shutdown -h now"
     print "SHUTTING DOWN"
     if not options.noshutdown and not options.test :
-        print os.system(toexec)
+        #print os.system(toexec)
+        pass
 
     exit(0)
 
