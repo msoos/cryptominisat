951c5dfe5e6265b9c6db5c265367a946d192465c
Mate Soos <soos.mate@gmail.com>
2015-04-29 22:18:29 +0100

    Fixing up version and sha tag

--- cryptominisat4/GitSHA1.h
+++ cryptominisat4/GitSHA1.h
@@ -21,6 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ***************************************************************/
 
+namespace CMSat {
 const char* get_git_version_sha1();
 const char* get_git_version_tag();
-
+}
