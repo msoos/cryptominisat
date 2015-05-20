ef27442a75aaffa8309f23d47a75196588d28a4b
Mate Soos <soos.mate@gmail.com>
2015-05-20 09:32:56 +0100

    Fixing shutdown behaviour on local run of AWS scripts

--- scripts/aws/local_test.sh
+++ scripts/aws/local_test.sh
@@ -5,6 +5,6 @@ DIR=`pwd`/../../../
 echo  `pwd`/../../tests/cnf-files > todo
 find  `pwd`/../../tests/cnf-files/ -printf "%f\n" | grep ".cnf$" >> todo
 
-./server.py --noaws --dir "$DIR" -t 3 --extratime 2 --cnfdir todo &
+./server.py --noaws --dir "$DIR" --noshutdown -t 3 --extratime 2 --cnfdir todo &
 ./client.py --noaws --dir "$DIR" --noshutdown --host localhost --temp /tmp/ --net lo --threads 2
 wait
