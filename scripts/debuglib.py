8862027a0d057aa5a50bae4f741a03216b3a27cd
Mate Soos <soos.mate@gmail.com>
2015-05-22 01:56:44 +0100

    A bit of cleanup, adding copyright headers

--- scripts/debuglib.py
+++ scripts/debuglib.py
@@ -1,4 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014  Mate Soos
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
 import random
+
 
 def get_max_var_from_clause(line):
     maxvar = 0
@@ -17,6 +38,7 @@ def get_max_var_from_clause(line):
         maxvar = max(maxvar, abs(num))
 
     return maxvar
+
 
 class debuglib:
     @staticmethod
