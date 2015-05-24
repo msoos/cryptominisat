2be0d62aa945771e6451a470c00bf3d1f95f3456
Mate Soos <soos.mate@gmail.com>
2015-05-24 22:12:02 +0100

    Cleaning up build script

--- scripts/aws/build.sh
+++ scripts/aws/build.sh
@@ -1,51 +1,28 @@
 #!/bin/bash
+set -e
+
 cd /home/ubuntu/cryptominisat
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit $rc
-fi
 echo "cd-d into cmsat"
 
+git checkout master
+echo "pulled"
+
 git pull
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit $rc
-fi
 echo "pulled"
 
 git checkout $1
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit $rc
-fi
 echo "got revision $1"
 
 cd build
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit $rc
-fi
 echo "cd-d into build"
 
 rm -rf C* c*
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit $rc
-fi
 echo "deleted build"
 
 cmake ..
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit $rc
-fi
 echo "cmake-d"
 
 make -j$2
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit $rc
-fi
 echo "built"
 
 exit 0
