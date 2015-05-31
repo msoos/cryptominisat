52b0eb4cba1287380fcec37e6e30ccaf8fb2883a
Mate Soos <soos.mate@gmail.com>
2015-05-31 23:16:33 +0100

    Changing dir before executing server.py in pre-server.py

--- scripts/aws/pre-server.py
+++ scripts/aws/pre-server.py
@@ -8,5 +8,6 @@ todo = boto.utils.get_instance_userdata()
 if todo == "":
     exit(0)
 
+os.chdir("/home/ubuntu/cryptominisat/scripts/aws/")
 command = "nohup /home/ubuntu/cryptominisat/scripts/aws/server.py %s &" % todo
 os.system(command)
