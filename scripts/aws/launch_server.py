ab2f9027fccb4edec50765fa7a41f0b64da612ba
Mate Soos <soos.mate@gmail.com>
2015-05-31 20:33:49 +0100

    Fixing server launcher

--- scripts/aws/launch_server.py
+++ scripts/aws/launch_server.py
@@ -12,10 +12,13 @@ else:
 
 conn = boto.ec2.connect_to_region("us-west-2")
 conn.run_instances(
-        'ami-4b69577b',
-        key_name='controlkey',
-        instance_type='t1.micro',
-        security_groups=['sg-507b3f35'],
-        instance_initiated_shutdown_behavior = 'terminate',
+        min_count = 1,
+        max_count = 1,
+        image_id = 'ami-4b69577b',
+        subnet_id = "subnet-88ab16ed",
+        instance_type='t2.micro',
         instance_profile_arn = 'arn:aws:iam::907572138573:instance-profile/server',
-        user_data=data)
+        user_data=data,
+        key_name='controlkey',
+        security_group_ids=['sg-507b3f35'],
+        instance_initiated_shutdown_behavior = 'terminate')
