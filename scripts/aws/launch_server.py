b8daab3a1d58d47882820943e5ce885008756931
Mate Soos <soos.mate@gmail.com>
2015-06-06 01:11:33 +0100

    Better launch server, with confirmation and option printing

--- scripts/aws/launch_server.py
+++ scripts/aws/launch_server.py
@@ -4,11 +4,32 @@
 import sys
 import boto.ec2
 
+
+def get_answer():
+    yes = set(['yes', 'y', 'ye', ''])
+    no = set(['no', 'n'])
+
+    choice = raw_input().lower()
+    if choice in yes:
+        return True
+    elif choice in no:
+        return False
+    else:
+        sys.stdout.write("Please respond with 'yes' or 'no'\n")
+        exit(0)
+
 data = " ".join(sys.argv[1:])
 if len(sys.argv) > 1:
-    print "Launching with data: '%s'" % data
+    print "Launching with data: %s" % data
 else:
     print "Launching without any parameters"
+
+sys.stdout.write("Is this OK? [y/n]? ")
+if not get_answer():
+    print "Aborting"
+    exit(0)
+
+print "Executing!"
 
 conn = boto.ec2.connect_to_region("us-west-2")
 conn.run_instances(
