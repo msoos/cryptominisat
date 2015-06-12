3c3ed92ebda409a7fb7305707efdb016d7a61ed6
Mate Soos <soos.mate@gmail.com>
2015-06-12 02:11:00 +0100

    Fixing up SWdia5by script

--- scripts/aws/build_swdia5by.sh
+++ scripts/aws/build_swdia5by.sh
@@ -1,5 +1,6 @@
+cd /home/ubuntu/
 aws s3 cp s3://msoos-solve-data/solvers/SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5.tar.gz . --region=us-west-2
-tar zvf SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5.tar.gz
+tar xzvf SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5.tar.gz
 cd SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5
 ./build.sh
 
