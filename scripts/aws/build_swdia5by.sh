06e315894cc5a6674c8093bedf73e1d734767a95
Mate Soos <soos.mate@gmail.com>
2015-06-12 21:34:47 +0100

    Fixing up some commented-out code

--- scripts/aws/build_swdia5by.sh
+++ scripts/aws/build_swdia5by.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+set -e
+
 cd /home/ubuntu/
 aws s3 cp s3://msoos-solve-data/solvers/SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5.tar.gz . --region=us-west-2
 tar xzvf SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5.tar.gz
 cd SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5
 ./build.sh
 
+# binary is now at:
+# --solver SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5/binary/SWDiA5BY_static
+
 cd /home/ubuntu/
-# now binary is at : /home/ubuntu/SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5/binary/SWDiA5BY_static
