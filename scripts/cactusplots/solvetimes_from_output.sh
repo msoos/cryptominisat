d2e3ed25576f01ba2cea2061236a2dbc4b86403e
Mate Soos <soos.mate@gmail.com>
2015-06-14 17:08:33 +0100

    Fixing scripts to handle data from AWS

--- scripts/cactusplots/solvetimes_from_output.sh
+++ scripts/cactusplots/solvetimes_from_output.sh
@@ -2,7 +2,7 @@
 
 zgrep "Total" *stdout.gz | awk '{print $5}' > solveTimes
 zgrep "s.*SATISFIABLE" *stdout.gz | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved
-ls *stdout.gz | sed 's/gz.*/gz/' > allFiles
+ls *stdout* | sed 's/gz.*/gz/' > allFiles
 zgrep "s UNSATISFIABLE" *stdout.gz | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT
 zgrep "s SATISFIABLE" *stdout.gz   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT
 #grep "v-elim" * | awk 'BEGIN {litrem=0;clauserem=0;velim=0;time=0;} {litrem+=$4; clauserem+=$6; varelim+=$8; time+=$12} END {print "time:             " time; print "lits removed:     " litrem; print "clauses subsumed: " clauserem; print "vars elimed:      " varelim}' > varelimed
