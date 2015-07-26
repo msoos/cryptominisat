083d2a9e8b819f67e686358b425af161ff61859a
Mate Soos <soos.mate@gmail.com>
2015-07-26 22:04:10 +0100

    Upload to AWS through reconf generator script

--- scripts/reconf/generate_reconf.sh
+++ scripts/reconf/generate_reconf.sh
@@ -1,5 +1,7 @@
-#/bin/bash
+#!/bin/bash
+
 set -e
+
 rm -f outs/*
 ./reconf.py -n 13 -f outs/out  ~/media/sat/out/satcomp091113/reconf*/*stdout* > output
 for i in $(seq 0 12); do
@@ -8,4 +10,13 @@ for i in $(seq 0 12); do
     c5.0 -f outs/out${i} -r > outs/out${i}.c50.out
 done
 ./tocpp.py -n 13 > ../../cryptominisat4/features_to_reconf.cpp
-
+read -r -p "Upload to AWS? [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY])
+        aws s3 cp ../../cryptominisat4/features_to_reconf.cpp s3://msoos-solve-data/solvers/
+        echo "Uploded to AWS"
+        ;;
+    *)
+        echo "Not uploaded to AWS"
+        ;;
+esac
