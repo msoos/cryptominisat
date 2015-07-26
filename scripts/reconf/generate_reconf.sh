efc1251ba208296188ae5f393ecac5336a767576
Mate Soos <soos.mate@gmail.com>
2015-07-26 22:00:10 +0100

    There are 13 reconfs now, update generator

--- scripts/reconf/generate_reconf.sh
+++ scripts/reconf/generate_reconf.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 set -e
 rm -f outs/*
-./reconf.py -n 13 -f outs/out  ~/media/sat/out/satcomp091113/reconf*/*stdout* > /dev/null
+./reconf.py -n 13 -f outs/out  ~/media/sat/out/satcomp091113/reconf*/*stdout* > output
 for i in $(seq 0 12); do
     echo "reconf with $i"
     cp reconf.names outs/out${i}.names
-    c5.0 -f outs/out${i} -r > outs/reconf${i}
+    c5.0 -f outs/out${i} -r > outs/out${i}.c50.out
 done
-./tocpp.py -n 12 > ../../cryptominisat4/features_to_reconf.cpp
+./tocpp.py -n 13 > ../../cryptominisat4/features_to_reconf.cpp
 
