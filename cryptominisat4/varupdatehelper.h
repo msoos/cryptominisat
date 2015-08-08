1625c39d2486c6aae10d03abaece4566e10feb8b
Mate Soos <soos.mate@gmail.com>
2015-08-08 16:14:30 +0100

    Updating copyright headers to state 2015

--- cryptominisat4/varupdatehelper.h
+++ cryptominisat4/varupdatehelper.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2014, Mate Soos. All rights reserved.
+ * Copyright (c) 2009-2015, Mate Soos. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
