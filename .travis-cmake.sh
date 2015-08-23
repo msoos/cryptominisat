e0b8f800406c20ff049d15e5eca8500fe74ea947
Mate Soos <soos.mate@gmail.com>
2015-08-23 17:19:38 +0100

    Don't use the obsolete "-f" flag for fuzz test

--- .travis-cmake.sh
+++ .travis-cmake.sh
@@ -185,7 +185,7 @@ esac
 #do fuzz testing
 if [ "$CMS_CONFIG" != "ONLY_SIMPLE" ] && [ "$CMS_CONFIG" != "AWS" ] && [ "$CMS_CONFIG" != "WEB" ] && [ "$CMS_CONFIG" != "PYTHON" ] && [ "$CMS_CONFIG" != "COVERAGE" ]; then
     cd ../scripts/
-    ./fuzz_test.py -f --novalgrind --fuzzlim 30
+    ./fuzz_test.py --novalgrind --fuzzlim 30
 fi
 
 cd ..
