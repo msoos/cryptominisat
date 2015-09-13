3c776c4c588bb621daa5c92bcfccec006d8db8d7
Mate Soos <soos.mate@gmail.com>
2015-09-13 23:45:14 +0100

    Removing 2 compiler warnings about unhandled cases

--- src/searcher.cpp
+++ src/searcher.cpp
@@ -368,6 +368,13 @@ void Searcher::debug_print_resolving_clause(const PropBy confl) const
             break;
         }
 
+        case xor_t: {
+            //in the future, we'll have XOR clauses. Not yet.
+            assert(false);
+            exit(-1);
+            break;
+        }
+
         case null_clause_t: {
             assert(false);
             break;
