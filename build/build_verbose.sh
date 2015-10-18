62f5932a201271b77973a20fad6dbf806452768c
Mate Soos <soos.mate@gmail.com>
2015-10-19 00:42:54 +0100

    Better build scripts

--- build/build_verbose.sh
+++ build/build_verbose.sh
@@ -5,8 +5,8 @@ set -e
 rm -rf cm* CM* lib* cryptomini* Testing* tests* py-lib include
 rm -rf tests
 cmake -DENABLE_TESTING=ON ..
-make -j4
-make test
+make -j4 VERBOSE=1
+ctest -V
 cd ../tests/simp-checks/
 ./checks.py -l ../../build/cryptominisat4 testfiles/*
 cd ../../build/
