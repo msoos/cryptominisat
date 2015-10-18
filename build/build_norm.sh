9a9f5202c8d91578f3a9be1d9d1d9f3606806e4f
Mate Soos <soos.mate@gmail.com>
2015-10-19 00:13:19 +0100

    Do the tests in verbose mode when compiling

--- build/build_norm.sh
+++ build/build_norm.sh
@@ -3,9 +3,10 @@
 set -e
 
 rm -rf cm* CM* lib* cryptomini* Testing* tests* py-lib include
+rm -rf tests
 cmake -DENABLE_TESTING=ON ..
 make -j4 VERBOSE=1
-make test
+ctest -V
 cd ../tests/simp-checks/
 ./checks.py -l ../../build/cryptominisat4 testfiles/*
 cd ../../build/
