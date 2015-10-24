bf214fac58ec87288bd9ae721c3b9acd63c29410
Mate Soos <soos.mate@gmail.com>
2015-10-24 18:17:27 +0100

    Fixing test locations + adding initial vrepl test

---
+++ tests/vrepl_test.cpp
@@ -0,0 +1,54 @@
+/*
+ * CryptoMiniSat
+ *
+ * Copyright (c) 2009-2015, Mate Soos. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.0 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+*/
+
+#include "gtest/gtest.h"
+
+#include <fstream>
+
+#include "src/solver.h"
+#include "src/varreplacer.h"
+#include "src/solverconf.h"
+using namespace CMSat;
+#include "test_helper.h"
+
+TEST(scc_test, find_1)
+{
+    SolverConf conf;
+    conf.doCache = false;
+
+    Solver s(&conf, new bool);
+    s.new_vars(20);
+    s.add_clause_outer(str_to_cl("1, 2"));
+    s.add_clause_outer(str_to_cl("-1, -2"));
+
+    s.add_clause_outer(str_to_cl("1, 3, 4, 5"));
+    s.add_clause_outer(str_to_cl("2, 3, 4, 5"));
+
+    VarReplacer repl(&s);
+    repl.new_vars(20);
+    repl.replace_if_enough_is_found();
+    EXPECT_EQ(repl.get_num_replaced_vars(), 1);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
