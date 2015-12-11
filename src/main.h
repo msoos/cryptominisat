15a7b24b35364dfe0aa202db8b9e3f1cd1c08b0c
Mate Soos <soos.mate@gmail.com>
2015-12-11 23:17:44 +0000

    Refactoring Main: new executable created, CUPS

--- src/main.h
+++ src/main.h
@@ -59,10 +59,12 @@ class Main
         }
 
         void parseCommandLine();
-        int solve();
-        int UniSolve();
+        virtual int solve();
 
     private:
+        //arguments
+        int argc;
+        char** argv;
         string typeclean;
         string var_elim_strategy;
         string drupfilname;
@@ -82,7 +84,9 @@ class Main
         po::options_description help_options_simple;
         po::options_description help_options_complicated;
 
+    protected:
         SATSolver* solver = NULL;
+        SolverConf conf;
 
         //File reading
         void readInAFile(SATSolver* solver2, const string& filename);
@@ -101,76 +105,22 @@ class Main
         std::ofstream* resultfile = NULL;
 
         //Config
-        SolverConf conf;
         bool zero_exit_status = false;
         std::string resultFilename;
-
         std::string debugLib;
         int printResult = true;
         string commandLine;
         unsigned num_threads = 1;
-
-        //Multi-start solving
         uint32_t max_nr_of_solutions = 1;
 
         //Files to read & write
         bool fileNamePresent;
         vector<string> filesToRead;
 
-        //Command line arguments
-        int argc;
-        char** argv;
-
         //Drup checker
         std::ostream* drupf = NULL;
         bool drupDebug = false;
 
-        //Unistuff
-        SATCount ApproxMC(
-            SATSolver* solver
-            , FILE* resLog
-            , std::mt19937& randomEngine
-        );
-
-        uint32_t UniGen(
-            uint32_t samples
-            , SATSolver* solver
-            , FILE* resLog, uint32_t sampleCounter
-            , std::mt19937& randomEngine
-            , std::map< string, uint32_t >& solutionMap
-            , uint32_t* lastSuccessfulHashOffset, double timeReference
-        );
-
-        bool AddHash(uint32_t clausNum, SATSolver* s, vector<Lit>& assumptions
-            , std::mt19937& randomEngine);
-        int32_t BoundedSATCount(uint32_t maxSolutions, SATSolver* solver
-            , vector<Lit>& assumptions
-        );
-        lbool BoundedSAT(
-            uint32_t maxSolutions, uint32_t minSolutions, SATSolver* solver
-            , vector<Lit>& assumptions, std::mt19937& randomEngine
-            , std::map<std::string, uint32_t>& solutionMap
-            , uint32_t* solutionCount
-        );
-        bool GenerateRandomBits(string& randomBits
-            , uint32_t size
-            , std::mt19937& randomEngine
-        );
-        uint32_t SolutionsToReturn(uint32_t minSolutions);
-        int GenerateRandomNum(int maxRange, std::mt19937& randomEngine);
-        bool printSolutions(FILE* res);
-        void SeedEngine(std::mt19937& randomEngine);
-        int singleThreadUniGenCall(uint32_t samples
-            , FILE* resLog, uint32_t sampleCounter
-            , std::map<std::string, uint32_t>& solutionMap
-            , std::mt19937& randomEngine
-            , uint32_t* lastSuccessfulHashOffset
-            , double timeReference
-        );
-
-        double startTime;
-        std::map< std::string, std::vector<uint32_t>> globalSolutionMap;
-        bool openLogFile(FILE*& res);
         vector<uint32_t> independent_vars;
 };
 
