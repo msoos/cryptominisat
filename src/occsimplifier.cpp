1829bd0d03450a2f2104c45f0ecf282214d9e0c4
Mate Soos <soos.mate@gmail.com>
2015-12-12 01:05:24 +0000

    Use our new recursive XOR code for Gauss detection

--- src/occsimplifier.cpp
+++ src/occsimplifier.cpp
@@ -894,7 +894,11 @@ bool OccSimplifier::execute_simplifier_strategy(const string& strategy)
                 #ifdef USE_GAUSS
                 XorFinder finder(this, solver);
                 finder.find_xors();
-                finder.add_xors_to_gauss();
+                finder.recursively_xor_xors();
+                const bool ok = finder.add_new_truths_from_xors();
+                if (ok) {
+                    finder.add_xors_to_gauss();
+                }
                 #endif
             }
         } else if (token == "occ-clean-implicit") {
