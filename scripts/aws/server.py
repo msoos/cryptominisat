5bfb09670aa3c5b295240cb13392a162e74b5633
Mate Soos <soos.mate@gmail.com>
2016-01-25 19:10:36 +0000

    Fixing shutdown on error from client

--- scripts/aws/server.py
+++ scripts/aws/server.py
@@ -366,7 +366,7 @@ class Server (threading.Thread):
                 self.handle_done(conn, cli_addr, data)
 
             if data["command"] == "error":
-                something_failed = True
+                shutdown(-1)
                 raise
 
             elif data["command"] == "need":
@@ -429,8 +429,7 @@ class Listener (threading.Thread):
             exc_type, exc_value, exc_traceback = sys.exc_info()
             the_trace = traceback.format_exc().rstrip().replace("\n", " || ")
             logging.error("Cannot listen on stocket! Traceback: %s", the_trace)
-            something_failed = True
-            exit(1)
+            shutdown(-1)
             raise
         while True:
             self.handle_one_connection()
@@ -445,7 +444,7 @@ class SpotManager (threading.Thread):
     def run(self):
         while True:
             try:
-                if (not something_failed) and (not server.ready_to_shutdown()):
+                if not server.ready_to_shutdown():
                     self.spot_creator.create_spots_if_needed()
             except:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -479,7 +478,6 @@ set_up_logging()
 logging.info("Server called with parameters: %s",
              pprint.pformat(options, indent=4).replace("\n", " || "))
 
-something_failed = False
 if not options.git_rev:
     options.git_rev = get_revision()
     logging.info("Revision not given, taking HEAD: %s", options.git_rev)
@@ -496,7 +494,7 @@ server.start()
 time.sleep(20)
 spotmanager.start()
 
-while threading.active_count() > 0 and not something_failed:
+while threading.active_count() > 0:
     time.sleep(0.5)
     if last_termination_sent is not None and server.ready_to_shutdown():
         diff = time.time() - last_termination_sent
