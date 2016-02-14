97f7dceb95e052104f6f6a050b8c81beb5f73b23
Mate Soos <soos.mate@gmail.com>
2016-02-14 18:08:49 +0000

    Adding drat-trim2 to AWS

--- scripts/aws/RequestSpotClient.py
+++ scripts/aws/RequestSpotClient.py
@@ -57,6 +57,7 @@ sudo -H -u ubuntu bash -c 'aws s3 cp s3://msoos-solve-data/solvers/email.conf . 
 sudo -H -u ubuntu bash -c '/home/ubuntu/cryptominisat/scripts/aws/build_swdia5by.sh >> /home/ubuntu/build.log'
 sudo -H -u ubuntu bash -c '/home/ubuntu/cryptominisat/scripts/aws/build_swdia5by_old.sh >> /home/ubuntu/build.log'
 sudo -H -u ubuntu bash -c '/home/ubuntu/cryptominisat/scripts/aws/build_lingeling_ayv.sh >> /home/ubuntu/build.log'
+sudo -H -u ubuntu bash -c '/home/ubuntu/cryptominisat/scripts/aws/build_drat-trim2.sh >> /home/ubuntu/build.log'
 
 # Start client
 cd /home/ubuntu/cryptominisat
