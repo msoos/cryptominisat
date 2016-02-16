cab11de27f65804f030e234ee8ad539a102f5489
Mate Soos <soos.mate@gmail.com>
2016-02-16 21:59:18 +0000

    Moving gen_cnf script to cnf-utils where it belongs

--- scripts/gen_cnf.py
+++
@@ -1,8 +0,0 @@
-def gen_cnf(n):
-    A = random_matrix(GF(2),n,n+1)
-    B = BooleanPolynomialRing(n,'x')
-    v = Matrix(B, n+1, 1, B.gens() + (1,))
-    l = (A*v).list()
-    a2 = ANFSatSolver(B)
-    s = a2.cnf(l)
-    open("matrix%02d.cnf"%n,"w").write(s)
