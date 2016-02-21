30aff25b2153f13e16438543a233cf072406892f
Mate Soos <soos.mate@gmail.com>
2016-02-21 00:19:05 +0000

    Refactoring server.py

--- scripts/aws/server.py
+++ scripts/aws/server.py
@@ -25,132 +25,6 @@ from common_aws import *
 import RequestSpotClient
 
 
-last_termination_sent = None
-
-
-def set_up_logging():
-    form = '[ %(asctime)-15s  %(levelname)s  %(message)s ]'
-
-    logformatter = logging.Formatter(form)
-
-    try:
-        os.unlink(options.logfile_name)
-    except:
-        pass
-    fileHandler = logging.FileHandler(options.logfile_name)
-    fileHandler.setFormatter(logformatter)
-    logging.getLogger().addHandler(fileHandler)
-
-    logging.getLogger().setLevel(logging.INFO)
-
-
-class PlainHelpFormatter(optparse.IndentedHelpFormatter):
-
-    def format_description(self, description):
-        if description:
-            return description + "\n"
-        else:
-            return ""
-
-usage = "usage: %prog"
-parser = optparse.OptionParser(usage=usage, formatter=PlainHelpFormatter())
-parser.add_option("--verbose", "-v", action="store_true",
-                  default=False, dest="verbose", help="Be more verbose"
-                  )
-
-parser.add_option("--port", "-p", default=10000, dest="port",
-                  help="Port to listen on. [default: %default]", type="int"
-                  )
-
-parser.add_option("--tout", "-t", default=2000, dest="timeout_in_secs",
-                  help="Timeout for the file in seconds"
-                  "[default: %default]",
-                  type=int
-                  )
-
-parser.add_option("--toutmult", default=12.1, dest="tout_mult",
-                  help="Approx: 1x is solving, 10x time is DRAT time wait, 1x is parsing, 0.1x that is sending us the result."
-                  "[default: %default]",
-                  type=float
-                  )
-
-
-parser.add_option("--memlimit", "-m", default=1600, dest="mem_limit_in_mb",
-                  help="Memory limit in MB"
-                  "[default: %default]",
-                  type=int
-                  )
-
-parser.add_option("--cnfdir", default="satcomp14", dest="cnf_dir",
-                  type=str,
-                  help="The list of CNF files to solve, first line the dir"
-                  "[default: %default]",
-                  )
-
-parser.add_option("--dir", default="/home/ubuntu/", dest="base_dir", type=str,
-                  help="The home dir of cryptominisat"
-                  )
-
-parser.add_option("--solver",
-                  default="cryptominisat/build/cryptominisat4",
-                  dest="solver",
-                  help="Solver executable"
-                  "[default: %default]",
-                  type=str
-                  )
-# other possibilities:
-#
-# SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5/binary/SWDiA5BY_static
-
-# NOTE: it's a ubuntu 14.04 on AWS, with sqlite3 fully installed
-
-
-parser.add_option("--s3bucket", default="msoos-solve-results",
-                  dest="s3_bucket", help="S3 Bucket to upload finished data"
-                  "[default: %default]",
-                  type=str
-                  )
-
-parser.add_option("--s3folder", default="results", dest="s3_folder",
-                  help="S3 folder name to upload data"
-                  "[default: %default]",
-                  type=str
-                  )
-
-parser.add_option("--git", dest="git_rev", type=str,
-                  help="The GIT revision to use. Default: HEAD"
-                  )
-
-parser.add_option("--opt", dest="extra_opts", type=str, default="",
-                  help="Extra options to give to solver"
-                  "[default: %default]",
-                  )
-
-parser.add_option("--noshutdown", "-n", default=False, dest="noshutdown",
-                  action="store_true", help="Do not shut down clients"
-                  )
-
-parser.add_option("--noaws", default=False, dest="noaws",
-                  action="store_true", help="Use AWS"
-                  )
-
-parser.add_option("--logfile", dest="logfile_name", type=str,
-                  default="python_server_log.txt", help="Name of LOG file")
-
-
-# parse options
-(options, args) = parser.parse_args()
-
-
-def rnd_id():
-    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
-
-options.logfile_name = options.base_dir + options.logfile_name
-options.s3_folder += "-" + time.strftime("%d-%B-%Y")
-options.s3_folder += "-%s" % rnd_id()
-final_s3_folder = options.s3_folder
-
-
 def get_revision():
     _, solvername = os.path.split(options.base_dir + options.solver)
     if solvername != "cryptominisat4":
@@ -192,12 +66,8 @@ class ToSolve:
     def __str__(self):
         return "%s (num: %d)" % (self.name, self.num)
 
-global acc_queue
-acc_queue = Queue.Queue()
-
 
 class Server (threading.Thread):
-
     def __init__(self):
         threading.Thread.__init__(self)
         self.files_available = []
@@ -503,32 +373,159 @@ So long and thanks for all the fish!
 
     exit(exitval)
 
-set_up_logging()
-logging.info("Server called with parameters: %s",
-             pprint.pformat(options, indent=4).replace("\n", " || "))
 
-if not options.git_rev:
-    options.git_rev = get_revision()
-    logging.info("Revision not given, taking HEAD: %s", options.git_rev)
+def parse_arguments():
+    class PlainHelpFormatter(optparse.IndentedHelpFormatter):
 
-server = Server()
-listener = Listener()
-spotmanager = SpotManager(options.noshutdown)
-listener.setDaemon(True)
-server.setDaemon(True)
-spotmanager.setDaemon(True)
+        def format_description(self, description):
+            if description:
+                return description + "\n"
+            else:
+                return ""
 
-listener.start()
-server.start()
-time.sleep(20)
-spotmanager.start()
+    usage = "usage: %prog"
+    parser = optparse.OptionParser(usage=usage, formatter=PlainHelpFormatter())
+    parser.add_option("--verbose", "-v", action="store_true",
+                      default=False, dest="verbose", help="Be more verbose"
+                      )
 
-while threading.active_count() > 0:
-    time.sleep(0.5)
-    if last_termination_sent is not None and server.ready_to_shutdown():
-        diff = time.time() - last_termination_sent
-        limit = 100
-        if diff > limit:
-            break
+    parser.add_option("--port", "-p", default=10000, dest="port",
+                      help="Port to listen on. [default: %default]", type="int"
+                      )
 
-shutdown()
+    parser.add_option("--tout", "-t", default=2000, dest="timeout_in_secs",
+                      help="Timeout for the file in seconds"
+                      "[default: %default]",
+                      type=int
+                      )
+
+    parser.add_option("--toutmult", default=12.1, dest="tout_mult",
+                      help="Approx: 1x is solving, 10x time is DRAT time wait, 1x is parsing, 0.1x that is sending us the result."
+                      "[default: %default]",
+                      type=float
+                      )
+
+
+    parser.add_option("--memlimit", "-m", default=1600, dest="mem_limit_in_mb",
+                      help="Memory limit in MB"
+                      "[default: %default]",
+                      type=int
+                      )
+
+    parser.add_option("--cnfdir", default="satcomp14", dest="cnf_dir",
+                      type=str,
+                      help="The list of CNF files to solve, first line the dir"
+                      "[default: %default]",
+                      )
+
+    parser.add_option("--dir", default="/home/ubuntu/", dest="base_dir", type=str,
+                      help="The home dir of cryptominisat"
+                      )
+
+    parser.add_option("--solver",
+                      default="cryptominisat/build/cryptominisat4",
+                      dest="solver",
+                      help="Solver executable"
+                      "[default: %default]",
+                      type=str
+                      )
+    # other possibilities:
+    #
+    # SWDiA5BY.alt.vd.res.va2.15000.looseres.3tierC5/binary/SWDiA5BY_static
+
+    parser.add_option("--s3bucket", default="msoos-solve-results",
+                      dest="s3_bucket", help="S3 Bucket to upload finished data"
+                      "[default: %default]",
+                      type=str
+                      )
+
+    parser.add_option("--s3folder", default="results", dest="s3_folder",
+                      help="S3 folder name to upload data"
+                      "[default: %default]",
+                      type=str
+                      )
+
+    parser.add_option("--git", dest="git_rev", type=str,
+                      help="The GIT revision to use. Default: HEAD"
+                      )
+
+    parser.add_option("--opt", dest="extra_opts", type=str, default="",
+                      help="Extra options to give to solver"
+                      "[default: %default]",
+                      )
+
+    parser.add_option("--noshutdown", "-n", default=False, dest="noshutdown",
+                      action="store_true", help="Do not shut down clients"
+                      )
+
+    parser.add_option("--noaws", default=False, dest="noaws",
+                      action="store_true", help="Use AWS"
+                      )
+
+    parser.add_option("--logfile", dest="logfile_name", type=str,
+                      default="python_server_log.txt", help="Name of LOG file")
+
+    # parse options
+    options, args = parser.parse_args()
+
+    def rnd_id():
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+
+    options.logfile_name = options.base_dir + options.logfile_name
+    options.s3_folder += "-" + time.strftime("%d-%B-%Y")
+    options.s3_folder += "-%s" % rnd_id()
+
+    return options, args
+
+
+def set_up_logging():
+    form = '[ %(asctime)-15s  %(levelname)s  %(message)s ]'
+    logformatter = logging.Formatter(form)
+
+    try:
+        os.unlink(options.logfile_name)
+    except:
+        pass
+    fileHandler = logging.FileHandler(options.logfile_name)
+    fileHandler.setFormatter(logformatter)
+    logging.getLogger().addHandler(fileHandler)
+    logging.getLogger().setLevel(logging.INFO)
+
+if __name__ == "__main__":
+    global options
+    global args
+    options, args = parse_arguments()
+
+    global acc_queue
+    acc_queue = Queue.Queue()
+    last_termination_sent = None
+
+    set_up_logging()
+    logging.info("Server called with parameters: %s",
+                 pprint.pformat(options, indent=4).replace("\n", " || "))
+
+    if not options.git_rev:
+        options.git_rev = get_revision()
+        logging.info("Revision not given, taking HEAD: %s", options.git_rev)
+
+    server = Server()
+    listener = Listener()
+    spotmanager = SpotManager(options.noshutdown)
+    listener.setDaemon(True)
+    server.setDaemon(True)
+    spotmanager.setDaemon(True)
+
+    listener.start()
+    server.start()
+    time.sleep(20)
+    spotmanager.start()
+
+    while threading.active_count() > 0:
+        time.sleep(0.5)
+        if last_termination_sent is not None and server.ready_to_shutdown():
+            diff = time.time() - last_termination_sent
+            limit = 100
+            if diff > limit:
+                break
+
+    shutdown()
