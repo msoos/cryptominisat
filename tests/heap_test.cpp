f3457401db1de686a29db1bfd9a943c10dfee08d
Mate Soos <soos.mate@gmail.com>
2016-03-26 17:25:50 +0000

    Fixing up heap

--- tests/heap_test.cpp
+++ tests/heap_test.cpp
@@ -25,6 +25,8 @@
 
 #include "src/heap.h"
 
+using CMSat::Heap;
+
 struct Comp
 {
     bool operator()(uint32_t a, uint32_t b) const
@@ -49,8 +51,8 @@ TEST(heap_minim, empty)
     Heap<Comp> heap(cmp);
     heap.insert(1);
     heap.insert(2);
-    EXPECT_EQ(heap.remove_min(), 1);
-    EXPECT_EQ(heap.remove_min(), 2);
+    EXPECT_EQ(heap.removeMin(), 1);
+    EXPECT_EQ(heap.removeMin(), 2);
     EXPECT_EQ( heap.heap_property(), true);
 }
 
@@ -61,9 +63,9 @@ TEST(heap_minim, empty2)
     heap.insert(1);
     heap.insert(2);
     heap.insert(3);
-    EXPECT_EQ(heap.remove_min(), 1);
-    EXPECT_EQ(heap.remove_min(), 2);
-    EXPECT_EQ(heap.remove_min(), 3);
+    EXPECT_EQ(heap.removeMin(), 1);
+    EXPECT_EQ(heap.removeMin(), 2);
+    EXPECT_EQ(heap.removeMin(), 3);
     EXPECT_EQ( heap.heap_property(), true);
 }
 
@@ -76,23 +78,8 @@ TEST(heap_minim, empty_lots)
         EXPECT_EQ( heap.heap_property(), true);
     }
     for(size_t i = 0; i < 100; i++) {
-        EXPECT_EQ(heap.remove_min(), i);
+        EXPECT_EQ(heap.removeMin(), i);
         EXPECT_EQ(heap.heap_property(), true);
-    }
-}
-
-TEST(heap_minim, copy_heap)
-{
-    Comp cmp;
-    Heap<Comp> heap(cmp);
-    for(size_t i = 0; i < 100; i++) {
-        heap.insert(99-i);
-        EXPECT_EQ( heap.heap_property(), true);
-    }
-    Heap<Comp> mycopy(heap);
-    for(size_t i = 0; i < 100; i++) {
-        EXPECT_EQ(mycopy.remove_min(), i);
-        EXPECT_EQ(mycopy.heap_property(), true);
     }
 }
 
@@ -102,8 +89,8 @@ TEST(heap_minim, inserted_inside)
     Heap<Comp> heap(cmp);
     heap.insert(10);
     heap.insert(20);
-    EXPECT_EQ(heap.in_heap(10), true);
-    EXPECT_EQ(heap.in_heap(20), true);
+    EXPECT_EQ(heap.inHeap(10), true);
+    EXPECT_EQ(heap.inHeap(20), true);
 }
 
 int main(int argc, char **argv) {
