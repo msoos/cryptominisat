2fb50a86916043eaa54d45c2efb0e854bd8bc9cc
Mate Soos <soos.mate@gmail.com>
2016-04-01 00:11:55 +0100

    Only check xor usage in case it's a gauss

--- src/distillerlongwithimpl.cpp
+++ src/distillerlongwithimpl.cpp
@@ -496,10 +496,12 @@ bool DistillerLongWithImpl::shorten_all_cl_with_cache_watch_stamp(
         }
 
         offset = clauses[i];
+        #ifdef USE_GAUS
         solver->cl_alloc.ptr(offset);
         if (cl->_used_in_xor) {
             goto copy;
         }
+        #endif
 
         if (sub_str_cl_with_cache_watch_stamp(offset, red, alsoStrengthen)) {
             solver->detachClause(offset);
