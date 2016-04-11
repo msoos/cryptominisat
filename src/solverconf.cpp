2d9fd66f2a59bd75a6dc8465c6220ff2b0ff5cd5
Mate Soos <soos.mate@gmail.com>
2016-04-11 19:36:29 +0100

    Using guess strategy 10 and cutting glues adjust at 0.2

--- src/solverconf.cpp
+++ src/solverconf.cpp
@@ -46,8 +46,8 @@ DLL_PUBLIC SolverConf::SolverConf() :
         , clause_decay(0.999)
         , min_time_in_db_before_eligible_for_cleaning(5ULL*1000ULL)
         , glue_must_keep_clause_if_below_or_eq(5)
-        , adjust_glue_if_too_many_low(2.00) //note: 1.0 should be enough in fact...
-        , guess_cl_effectiveness(false)
+        , adjust_glue_if_too_many_low(0.20)
+        , guess_cl_effectiveness(10)
 
         //Restarting
         , restart_first(100)
