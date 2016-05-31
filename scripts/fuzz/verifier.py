427c827fc6b9f7773ce48c6a56ac410405604103
Mate Soos <soos.mate@gmail.com>
2016-05-31 22:58:34 +0100

    Moving over all verification into verifier.py

--- scripts/fuzz/verifier.py
+++ scripts/fuzz/verifier.py
@@ -23,10 +23,156 @@ import optparse
 import gzip
 import re
 import fnmatch
+from xor_to_cnf_class import *
+from debuglib import *
+import subprocess
+import os
+import stat
+import time
+import resource
+
+
+def unique_file(fname_begin, fname_end=".cnf"):
+        counter = 1
+        while 1:
+            fname = fname_begin + '_' + str(counter) + fname_end
+            try:
+                fd = os.open(
+                    fname, os.O_CREAT | os.O_EXCL, stat.S_IREAD | stat.S_IWRITE)
+                os.fdopen(fd).close()
+                return fname
+            except OSError:
+                pass
+
+            counter += 1
+
+def setlimits():
+    # sys.stdout.write("Setting resource limit in child (pid %d): %d s\n" %
+    # (os.getpid(), options.maxtime))
+    resource.setrlimit(resource.RLIMIT_CPU, (options.maxtime, options.maxtime))
 
 class solution_parser:
-    def __init__(self):
+    def __init__(self, options):
+        self.options = options
         pass
+
+    @staticmethod
+    def test_found_solution(solution, fname, debugLibPart=None):
+        if debugLibPart is None:
+            print("Verifying solution for CNF file %s" % fname)
+        else:
+            print("Verifying solution for CNF file %s, part %d" %
+                  (fname, debugLibPart))
+
+        if fnmatch.fnmatch(fname, '*.gz'):
+            f = gzip.open(fname, "r")
+        else:
+            f = open(fname, "r")
+        clauses = 0
+        thisDebugLibPart = 0
+
+        for line in f:
+            line = line.rstrip()
+
+            # skip empty lines
+            if len(line) == 0:
+                continue
+
+            # count debug lib parts
+            if line[0] == 'c' and "Solver::solve" in line:
+                thisDebugLibPart += 1
+
+            # if we are over debugLibPart, exit
+            if debugLibPart is not None and thisDebugLibPart >= debugLibPart:
+                f.close()
+                return
+
+            # check solution against clause
+            if line[0] != 'c' and line[0] != 'p':
+                if line[0] != 'x':
+                    solution_parser._check_regular_clause(line, solution)
+                else:
+                    solution_parser._check_xor_clause(line, solution)
+
+                clauses += 1
+
+        f.close()
+        print("Verified %d original xor&regular clauses" % clauses)
+
+    def check_unsat(self, fname):
+        a = XorToCNF()
+        tmpfname = unique_file("tmp_for_xor_to_cnf_convert")
+        a.convert(fname, tmpfname)
+        # execute with the other solver
+        toexec = "lingeling -f %s" % tmpfname
+        print("Solving with other solver: %s" % toexec)
+        curr_time = time.time()
+        try:
+            p = subprocess.Popen(toexec.rsplit(),
+                                 stdout=subprocess.PIPE,
+                                 preexec_fn=setlimits)
+        except OSError:
+            print("ERROR: Probably you don't have lingeling installed!")
+            raise
+
+        consoleOutput2 = p.communicate()[0]
+        os.unlink(tmpfname)
+
+        # if other solver was out of time, then we can't say anything
+        diff_time = time.time() - curr_time
+        if diff_time > self.options.maxtime - self.options.maxtimediff:
+            print("Other solver: too much time to solve, aborted!")
+            return None
+
+        # extract output from the other solver
+        print("Checking other solver output...")
+        otherSolverUNSAT, otherSolverSolution, _ = self.parse_solution_from_output(
+            consoleOutput2.split("\n"))
+
+        # check if the other solver agrees with us
+        return otherSolverUNSAT
+
+    def check_debug_lib(self, fname):
+        largestPart = self._find_largest_debuglib_part(fname)
+        for debugLibPart in range(1, largestPart + 1):
+            fname_debug = "%s-debugLibPart%d.output" % (fname, debugLibPart)
+            print("Checking debug lib part %s -- %s " % (debugLibPart, fname_debug))
+
+            if (os.path.isfile(fname_debug) is False):
+                print("Error: Filename to be read '%s' is not a file!" % fname_debug)
+                exit(-1)
+
+            # take file into mem
+            f = open(fname_debug, "r")
+            text = f.read()
+            output_lines = text.splitlines()
+            f.close()
+
+            unsat, solution, conflict = self.parse_solution_from_output(output_lines)
+            assumps = self._get_assumps(fname, debugLibPart)
+            if unsat is False:
+                print("debugLib is SAT")
+                self._check_assumps_inside_solution(assumps, solution)
+                self.test_found_solution(solution, fname, debugLibPart)
+            else:
+                print("debugLib is UNSAT")
+                assert conflict is not None, "debugLibPart must create a conflict in case of UNSAT"
+                self._check_assumps_inside_conflict(assumps, conflict)
+                tmpfname = unique_file("tmp_for_extract_libpart")
+                self._extract_lib_part(fname, debugLibPart, assumps, tmpfname)
+
+                # check with other solver
+                ret = self.check_unsat(tmpfname)
+                if ret is None:
+                    print("Cannot check, other solver took too much time")
+                elif ret is True:
+                    print("UNSAT verified by other solver")
+                else:
+                    print("Grave bug: SAT-> UNSAT : Other solver found solution!!")
+                    exit(-1)
+                os.unlink(tmpfname)
+
+            os.unlink(fname_debug)
 
     @staticmethod
     def parse_solution_from_output(output_lines, ignoreNoSolution = False):
@@ -115,8 +261,120 @@ class solution_parser:
 
         return unsat, solution, conflict
 
+    def _extract_lib_part(self, fname, debug_num, assumps, tofile):
+        fromf = open(fname, "r")
+        thisDebugLibPart = 0
+        maxvar = 0
+        numcls = 0
+        for line in fromf:
+            line = line.strip()
+
+            # ignore empty strings and headers
+            if not line or line[0] == "p":
+                continue
+
+            # process (potentially special) comments
+            if line[0] == "c":
+                if "Solver::solve" in line:
+                    thisDebugLibPart += 1
+
+                continue
+
+            # break out if we reached the debug lib part
+            if thisDebugLibPart >= debug_num:
+                break
+
+            # count clauses and get max var number
+            numcls += 1
+            maxvar = max(maxvar, get_max_var_from_clause(line))
+
+        fromf.close()
+
+        # now we can create the new CNF file
+        fromf = open(fname, "r")
+        tof = open(tofile, "w")
+        tof.write("p cnf %d %d\n" % (maxvar, numcls + len(assumps)))
+
+        thisDebugLibPart = 0
+        for line in fromf:
+            line = line.strip()
+            # skip empty lines and headers
+            if not line or line[0] == "p":
+                continue
+
+            # parse up special header
+            if line[0] == "c":
+                if "Solver::solve" in line:
+                    thisDebugLibPart += 1
+
+                continue
+
+            # break out if we reached the debug lib part
+            if thisDebugLibPart >= debug_num:
+                break
+
+            tof.write(line + '\n')
+
+        # add assumptions
+        for lit in assumps:
+            tof.write("%d 0\n" % lit)
+
+        fromf.close()
+        tof.close()
+
+    def _get_assumps(self, fname, debugLibPart):
+        f = open(fname, "r")
+
+        thispart = 0
+        solveline = None
+        for line in f:
+            if "Solver::solve" in line:
+                thispart += 1
+                if thispart == debugLibPart:
+                    solveline = line
+                    break
+        f.close()
+
+        assert solveline is not None
+        ret = re.match("c.*Solver::solve\((.*)\)", solveline)
+        assert ret is not None
+        assumps = ret.group(1).strip().split()
+        assumps = [int(x) for x in assumps]
+
+        print("Assumptions: ", assumps)
+        return assumps
+
+    def _check_assumps_inside_conflict(self, assumps, conflict):
+        for lit in conflict:
+            if -1 * lit not in assumps:
+                print("ERROR: Final conflict contains %s but assumps is %s" %(conflict, assumps))
+                print("ERROR: lit ", lit, " is in conflict but its inverse is not is assumps!")
+                exit(-100)
+
+        print("OK, final conflict only contains elements from assumptions")
+
+    def _check_assumps_inside_solution(self, assumps, solution):
+        for lit in assumps:
+            var = abs(lit)
+            val = lit > 0
+            if var in solution:
+                if solution[var] != val:
+                    print("Solution pinted has literal %s but assumptions contained the inverse: '%s'" % (-1 * lit, assumps))
+                    exit(-100)
+
+        print("OK, all assumptions inside solution")
+
+    def _find_largest_debuglib_part(self, fname):
+        largestPart = 0
+        dirList2 = os.listdir(".")
+        for fname_debug in dirList2:
+            if fnmatch.fnmatch(fname_debug, "%s-debugLibPart*.output" % fname):
+                largestPart += 1
+
+        return largestPart
+
     @staticmethod
-    def check_regular_clause(line, solution):
+    def _check_regular_clause(line, solution):
             lits = line.split()
             final = False
             for lit in lits:
@@ -136,7 +394,7 @@ class solution_parser:
                 exit(100)
 
     @staticmethod
-    def check_xor_clause(line, solution):
+    def _check_xor_clause(line, solution):
         line = line.lstrip('x')
         lits = line.split()
         final = False
@@ -152,49 +410,6 @@ class solution_parser:
         if final is False:
             print("Error: xor-clause '%s' not satisfied." % line)
             exit(-1)
-
-    @staticmethod
-    def test_found_solution(solution, fname, debugLibPart=None):
-        if debugLibPart is None:
-            print("Verifying solution for CNF file %s" % fname)
-        else:
-            print("Verifying solution for CNF file %s, part %d" %
-                  (fname, debugLibPart))
-
-        if fnmatch.fnmatch(fname, '*.gz'):
-            f = gzip.open(fname, "r")
-        else:
-            f = open(fname, "r")
-        clauses = 0
-        thisDebugLibPart = 0
-
-        for line in f:
-            line = line.rstrip()
-
-            # skip empty lines
-            if len(line) == 0:
-                continue
-
-            # count debug lib parts
-            if line[0] == 'c' and "Solver::solve" in line:
-                thisDebugLibPart += 1
-
-            # if we are over debugLibPart, exit
-            if debugLibPart is not None and thisDebugLibPart >= debugLibPart:
-                f.close()
-                return
-
-            # check solution against clause
-            if line[0] != 'c' and line[0] != 'p':
-                if line[0] != 'x':
-                    solution_parser.check_regular_clause(line, solution)
-                else:
-                    solution_parser.check_xor_clause(line, solution)
-
-                clauses += 1
-
-        f.close()
-        print("Verified %d original xor&regular clauses" % clauses)
 
 def parse_arguments():
     class PlainHelpFormatter(optparse.IndentedHelpFormatter):
@@ -212,6 +427,11 @@ For example:
     parser = optparse.OptionParser(usage=usage, formatter=PlainHelpFormatter())
     parser.add_option("--verbose", "-v", action="store_true",
                       default=False, dest="verbose", help="Be more verbose")
+    parser.add_option("--tout", "-t", dest="maxtime", type=int, default=100,
+                  help="Max time to run. Default: %default")
+    parser.add_option("--textra", dest="maxtimediff", type=int, default=10,
+                  help="Extra time on top of timeout for processing."
+                  " Default: %default")
     # parse options
     options, args = parser.parse_args()
     return options, args
@@ -230,14 +450,27 @@ if __name__ == "__main__":
     print("Verifying CNF file '{cnf}' against solution in file '{sol}'".format(
         **{"cnf":cnf_file, "sol":sol_file}))
 
+    print("Checking debug libs...")
+    sol_parser = solution_parser(options)
+    sol_parser.check_debug_lib(cnf_file)
+
+    print("Checking console output...")
     sol = {}
     with open(sol_file) as f:
         dat = f.read()
 
     dat = dat.split("\n")
-    unsat, solution, conflict = solution_parser.parse_solution_from_output(dat)
-    if unsat:
-        print("Not checking UNSAT solution")
+    unsat, solution, _ = sol_parser.parse_solution_from_output(dat)
+    if not unsat:
+        sol_parser.test_found_solution(solution, cnf_file)
         exit(0)
 
-    solution_parser.test_found_solution(solution, cnf_file)
+    # check with other solver
+    ret = sol_parser.check_unsat(cnf_file)
+    if ret is None:
+        print("Other solver time-outed, cannot check")
+    elif ret is True:
+        print("UNSAT verified by other solver")
+    else:
+        print("Grave bug: SAT-> UNSAT : Other solver found solution!!")
+        exit(-1)
