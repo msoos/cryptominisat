392bcc7d5a1875911bf2ebabb3fbd5162e51ef89
Mate Soos <soos.mate@gmail.com>
2016-06-01 22:26:17 +0100

    Adding option to try to unset vars in ScalMC

--- src/scalmc.h
+++ src/scalmc.h
@@ -85,6 +85,7 @@ private:
     uint32_t tApproxMC = 17;
     uint32_t searchMode = 1;
     int      loopTimeout = 2500;
+    int      unset_vars = 0;
     std::ofstream cusp_logf;
     std::mt19937 randomEngine;
 };
