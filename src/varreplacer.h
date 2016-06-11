bfc49f299a5f4af7896f2abbc87171a8f1808f81
Mate Soos <soos.mate@gmail.com>
2016-06-11 13:09:21 +0100

    These are two conceptually very different operations. Use 2 funcs

--- src/varreplacer.h
+++ src/varreplacer.h
@@ -57,7 +57,8 @@ class VarReplacer
         void print_some_stats(const double global_cpu_time) const;
         const SCCFinder* get_scc_finder() const;
 
-        void extend_model_all(bool set_all = false);
+        void extend_model_already_set();
+        void extend_model_set_undef();
         void extend_model(const uint32_t var);
 
         uint32_t get_var_replaced_with(const uint32_t var) const;
