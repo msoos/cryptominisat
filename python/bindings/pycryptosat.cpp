f11efd9222db5b69b3db9cd99e9408b6213f4731
ysard <ysard_git@gmx.fr>
2017-02-28 11:54:30 +0100

    Fix copyright

--- python/bindings/pycryptosat.cpp
+++ python/bindings/pycryptosat.cpp
@@ -3,6 +3,7 @@ Python bindings to CryptoMiniSat (http://msoos.org)
 
 Copyright (c) 2013, Ilan Schnell, Continuum Analytics, Inc.
               2014, Mate Soos
+              2017, Pierre Vignet
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
