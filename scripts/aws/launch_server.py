6cac68cb6a0684f67a188e3c58faec21c626a1a9
Mate Soos <soos.mate@gmail.com>
2017-03-26 16:43:02 +0100

    PEP8-ing the server script

--- scripts/aws/launch_server.py
+++ scripts/aws/launch_server.py
@@ -11,6 +11,7 @@ import server_option_parser
 
 print("NOTE: you have to give options to cryptominisat as --opt \"--keepguess=1,--keepglue=4\"")
 
+
 def get_answer():
     yes = set(['yes', 'y', 'ye'])
     no = set(['no', 'n'])
@@ -97,13 +98,13 @@ DATA="%s"
 
 conn = boto.ec2.connect_to_region("us-west-2")
 conn.run_instances(
-        min_count = 1,
-        max_count = 1,
-        image_id = 'ami-a9e2da99', # Unbuntu 14.04 US-west (Oregon)
-        subnet_id = "subnet-88ab16ed",
-        instance_type='t2.micro',
-        instance_profile_arn = 'arn:aws:iam::907572138573:instance-profile/server',
-        user_data=cloud_init,
-        key_name='controlkey',
-        security_group_ids=['sg-507b3f35'],
-        instance_initiated_shutdown_behavior = 'terminate')
+    min_count=1,
+    max_count=1,
+    image_id='ami-a9e2da99',  # Unbuntu 14.04 US-west (Oregon)
+    subnet_id="subnet-88ab16ed",
+    instance_type='t2.micro',
+    instance_profile_arn='arn:aws:iam::907572138573:instance-profile/server',
+    user_data=cloud_init,
+    key_name='controlkey',
+    security_group_ids=['sg-507b3f35'],
+    instance_initiated_shutdown_behavior='terminate')
