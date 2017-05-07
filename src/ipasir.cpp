ef22ea74561efce89077c076543067a345777e96
Mate Soos <soos.mate@gmail.com>
2017-05-07 21:25:26 +0100

    Updating IPASIR

--- src/ipasir.cpp
+++ src/ipasir.cpp
@@ -226,3 +226,7 @@ DLL_PUBLIC void ipasir_set_terminate (void * /*solver*/, void * /*state*/, int (
 
 }
 
+DLL_PUBLIC void ipasir_set_learn (void * solver, void * state, int max_length, void (*learn)(void * state, int * clause))
+{
+    //this is complicated
+}
