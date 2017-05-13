373c026e527217611b4297833ad820f4b8d03a82
Mate Soos <soos.mate@gmail.com>
2017-05-13 19:51:47 +0100

    Add 1500s cutoff check into solvetimes

--- scripts/output_parser/solvetimes_from_output.sh
+++ scripts/output_parser/solvetimes_from_output.sh
@@ -6,10 +6,20 @@ zgrep --color -i error -- *stderr* *stderr*
 zgrep --color -i abort -- *stderr* *stderr*
 zgrep --color -i failed -- *stderr* *stderr*
 
-
-zgrep "Total" -- *stdout.gz | awk '{print $5}' > solveTimes
-zgrep "s.*SATISFIABLE" -- *stdout.gz | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved
 ls -- *stdout* | sed 's/gz.*/gz/' > allFiles
+
+# 1500 cutoff
+zgrep "Total" -- *stdout.gz | awk '{print $5}' > solveTimes
+zgrep "Total" -- *stdout.gz | awk '{if ($5 < 1500) {print $1}}' | sed 's/:c.*$//' | sort > solved_under_1500_full_list
+
+
+# for normal
+zgrep "s.*SATISFIABLE" -- *stdout.gz | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved
 zgrep "s UNSATISFIABLE" -- *stdout.gz | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT
 zgrep "s SATISFIABLE" -- *stdout.gz   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT
-#grep "v-elim" * | awk 'BEGIN {litrem=0;clauserem=0;velim=0;time=0;} {litrem+=$4; clauserem+=$6; varelim+=$8; time+=$12} END {print "time:             " time; print "lits removed:     " litrem; print "clauses subsumed: " clauserem; print "vars elimed:      " varelim}' > varelimed
+
+# 1500 cutoff
+zgrep "s.*SATISFIABLE" -- `cat solved_under_1500_full_list` | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved1500
+zgrep "s UNSATISFIABLE" -- `cat solved_under_1500_full_list` | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT1500
+zgrep "s SATISFIABLE" -- `cat solved_under_1500_full_list`   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT1500
+rm solved_under_1500_full_list
