257711727fad75e0116efbbd5fbd7d6dd570efdf
Mate Soos <soos.mate@gmail.com>
2017-05-29 16:38:19 +0100

    Fixing stats

--- src/clause.h
+++ src/clause.h
@@ -89,6 +89,16 @@ struct AtecedentData
         return *this;
     }
 
+    uint32_t sum_size() const
+    {
+        uint32_t sum = 0;
+        sum += binIrred*2;
+        sum += binRed*2;
+        sum += size_longs.get_sum();
+
+        return sum;
+    }
+
     T binRed = 0;
     T binIrred = 0;
     T longIrred = 0;
