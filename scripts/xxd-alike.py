ec380205e4ec5921dd8c0a685d738930aeee9901
Mate Soos <soos.mate@gmail.com>
2017-08-15 21:55:16 +0100

    Fixing off-by-one in xxd
    Related to issue #406

--- scripts/xxd-alike.py
+++ scripts/xxd-alike.py
@@ -30,8 +30,5 @@ with open(output_path, 'w') as out:
             out.write('\n  ')
         out.write('0x{:02x}'.format(convert(byte)))
 
-    out.write(', ')
-    out.write('0x00')
-
     out.write('\n};\n')
     out.write('unsigned int {}_len = {};\n'.format(output_name, len(contents)+1))
