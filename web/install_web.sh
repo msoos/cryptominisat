1c609426c12c83ac045f04ecb14716d49b79aa56
Mate Soos <soos.mate@gmail.com>
2017-10-01 00:31:48 +0100

    Use standard dygraphs

--- web/install_web.sh
+++ web/install_web.sh
@@ -5,11 +5,8 @@ set -e
 # set up dygraphs
 cd dygraphs
 npm install
-npm install gulp
-gulp dist
-gulp test
+npm run build
 cd ..
 
 # set up jquery
 wget http://code.jquery.com/jquery-1.11.3.min.js
-
