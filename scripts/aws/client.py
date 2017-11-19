fba32940484f35bd04c342ddd60f55ff32572b5b
Mate Soos <soos.mate@gmail.com>
2017-11-19 22:55:37 +0000

    Fixing some of the AWS script

--- scripts/aws/client.py
+++ scripts/aws/client.py
@@ -23,7 +23,7 @@ import functools
 import string
 
 # for importing in systems where "." is not in the PATH
-sys.path.append(os.getcwd())
+sys.path.append(os.getcwd()+"/script/learn/")
 from common_aws import *
 import add_lemma_ind as addlemm
 
