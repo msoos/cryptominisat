7a20ab2fdf35e4ae71fe74392e41a06f3ac0548b
Mate Soos <soos.mate@gmail.com>
2018-03-20 12:42:47 +0800

    Adding more checking for licenses

--- scripts/travis-cmake.sh
+++ scripts/travis-cmake.sh
@@ -40,11 +40,30 @@ check_license() {
     fi
 }
 
+check_license_fnames() {
+    #license check -- first print and then fail in case of problems
+    find $1 -type f -name $2 -exec utils/licensecheck/licensecheck.pl -m {} \;
+    NUM=$(find $1 -type f -name $2 -exec utils/licensecheck/licensecheck.pl -m {} \; | grep UNK | wc -l)
+    shopt -s extglob
+    NUM="${NUM##*( )}"
+    NUM="${NUM%%*( )}"
+    shopt -u extglob
+    if [ "$NUM" -ne 0 ]; then
+        echo "There are some files without license information!"
+        exit -1
+    fi
+}
+
+check_license CMakeLists.txt
 check_license ./src
 check_license ./tests/
 check_license ./scripts/fuzz/
 check_license ./scripts/learn/
 check_license ./scripts/aws/
+
+check_license_fnames tests/ CMakeLists.txt
+check_license_fnames src/ CMakeLists.txt
+check_license_fnames scripts/ CMakeLists.txt
 
 NUM=$(./utils/licensecheck/licensecheck.pl -m  ./tests | grep UNK | wc -l)
 shopt -s extglob
