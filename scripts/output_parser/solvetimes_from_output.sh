01c5d5fe64217e1c0aa34cc821dac3387a4fb9ef
Mate Soos <soos.mate@gmail.com>
2018-03-27 13:33:13 +0800

    solvetimes are non-gzipped

--- scripts/output_parser/solvetimes_from_output.sh
+++ scripts/output_parser/solvetimes_from_output.sh
@@ -2,27 +2,27 @@
 
 #set -x
 
-zgrep --color -i "assert.*fail" `ls *.out.gz`
-zgrep --color -i signal `ls *.out.gz`
-zgrep --color -i error `ls *.out.gz`
-zgrep --color -i terminate `ls *.out.gz`
-zgrep --color -i abort `ls *.out.gz` | grep -v expensive
-zgrep --color -i failed `ls *.out.gz` | grep -v probes
+grep --color -i "assert.*fail" `ls *.out`
+grep --color -i signal `ls *.out`
+grep --color -i error `ls *.out`
+grep --color -i terminate `ls *.out`
+grep --color -i abort `ls *.out` | grep -v expensive
+grep --color -i failed `ls *.out` | grep -v probes
 
-ls -- *out.gz | sed 's/gz.*/gz/' > allFiles
+ls -- *out | sed 's/gz.*/gz/' > allFiles
 
 # 1500 cutoff
-zgrep "Total" *out.gz | awk '{print $5}' > solveTimes
-zgrep "Total" *out.gz | awk '{if ($5 < 1500) {print $1}}' | sed 's/:c.*$//' | sort > solved_under_1500_full_list
+grep "Total" *out | awk '{print $5}' > solveTimes
+grep "Total" *out | awk '{if ($5 < 1500) {print $1}}' | sed 's/:c.*$//' | sort > solved_under_1500_full_list
 
 
 # for normal
-zgrep "s.*SATISFIABLE" *out.gz | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved
-zgrep "s UNSATISFIABLE" *out.gz | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT
-zgrep "s SATISFIABLE" *out.gz   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT
+grep "s.*SATISFIABLE" *out | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved
+grep "s UNSATISFIABLE" *out | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT
+grep "s SATISFIABLE" *out   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT
 
 # 1500 cutoff
-zgrep "s.*SATISFIABLE" $(cat solved_under_1500_full_list) | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved1500
-zgrep "s UNSATISFIABLE" $(cat solved_under_1500_full_list) | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT1500
-zgrep "s SATISFIABLE" $(cat solved_under_1500_full_list)   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT1500
+grep "s.*SATISFIABLE" $(cat solved_under_1500_full_list) | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved1500
+grep "s UNSATISFIABLE" $(cat solved_under_1500_full_list) | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT1500
+grep "s SATISFIABLE" $(cat solved_under_1500_full_list)   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT1500
 rm solved_under_1500_full_list
