e3c0e13d918e32b518e85728a0daaaf3a1c50536
Mate Soos <soos.mate@gmail.com>
2018-04-12 20:19:04 +0800

    Don't give the same option twice to CMS

--- scripts/fuzz/fuzz_test.py
+++ scripts/fuzz/fuzz_test.py
@@ -356,7 +356,7 @@ class Tester:
             opts = ["scc", "varelim", "comps", "strengthen", "probe", "intree",
                     "stamp", "cache", "otfsubsume",
                     "renumber", "savemem", "moreminim", "gates", "bva",
-                    "gorshort", "gandrem", "gateeqlit", "schedsimp", "presimp"]
+                    "gorshort", "gandrem", "gateeqlit", "schedsimp"]
 
             if "xor" in self.extra_options_if_supported:
                 opts.append("xor")
