74572a63171c62aa73b2ac861abcb91b1ca765dd
Mate Soos <soos.mate@gmail.com>
2018-04-14 16:43:51 +0800

    Cleaner implementation

--- scripts/reconf/count_plus_minus.sh
+++ scripts/reconf/count_plus_minus.sh
@@ -5,7 +5,7 @@
 #./reconf.py -n 18 -i 1,2,8,9,10,11,15,5,14,13,3 -f outs/out /home/soos/media/sat/out/new/out-reconf-6776906.wlm01-*/*.out
 
 
-for f in `ls outfile*.data`; do
+for f in `ls outs/*.data`; do
     echo "$f:"
     grep ",+" $f  | wc -l;
     grep ",-" $f  | wc -l;
@@ -14,19 +14,22 @@ done
 
 
 echo "this is solved by everyone:"
-grep "total-10-13-u.cnf" outfile*.data
+grep "total-10-13-u.cnf" outs/*.data
 
 echo "this is solved by nobody:"
-grep "partial-10-13-u.cnf" outfile*.data
+grep "partial-10-13-u.cnf" outs/*.data
 
-echo "this is solved by some only, small diff:"
-grep "mp1-22.5.cnf.gz" outfile*.data
+echo "this is solved by some only -8-==rec15 and -10-=rec17 and -9-=rec16), small diff:"
+grep "mp1-22.5.cnf.gz" outs/*.data
 
 echo "this is solved by everyone except 7, large diff:"
-grep "mp1-klieber2017s-1000-024-eq.cnf" outfile*.data
+grep "mp1-klieber2017s-1000-024-eq.cnf" outs/*.data
 
-echo "given    neg : reconf17, reconf7, reconf16"
-echo "which is eqiv: 10. 4, 9, (note: 8-drat = 15, which is ignored)"
+echo "given    neg : rec17, rec7, rec16"
+echo "which is eqiv: 10, 4, 9, (note: 8-drat = 15, which is ignored)"
+
+echo "to check:"
+echo 'grep -i "User time"  */mp1-klieber2017s-1000-024-eq.cnf*.timeout | awk '{print $5 " -- " $1}' | sort -n'
 
 echo 'mapping:
 num -> reconf
