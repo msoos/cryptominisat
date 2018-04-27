b3452b8bf30ac2bb3ba47c20b214052c14a151fa
Mate Soos <soos.mate@gmail.com>
2018-04-27 17:49:13 +0800

    Don't allow XOR connecting vars to be eliminated in GAUSS

--- src/cryptominisat.cpp
+++ src/cryptominisat.cpp
@@ -534,6 +534,7 @@ DLL_PUBLIC void SATSolver::set_allow_otf_gauss()
         s.conf.reconfigure_val = 15;
         s.conf.gaussconf.max_num_matrixes = 10;
         s.conf.gaussconf.autodisable = false;
+        s.conf.allow_elim_xor_vars = false;
     }
     #endif
 }
