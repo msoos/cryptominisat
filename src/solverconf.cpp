c6e9d97a81a79399a06719be8bed387f9185b04a
Mate Soos <soos.mate@gmail.com>
2018-04-30 11:51:43 +0800

    Don't change default behaviour for GJ

--- src/solverconf.cpp
+++ src/solverconf.cpp
@@ -36,27 +36,15 @@ DLL_PUBLIC SolverConf::SolverConf() :
         , polarity_mode(PolarityMode::polarmode_automatic)
 
         //Clause cleaning
-        #ifndef USE_GAUSS
         , every_lev1_reduce(10000) // kept for a while then moved to lev2
         , every_lev2_reduce(15000) // cleared regularly
         , must_touch_lev1_within(30000)
-        #else
-        , every_lev1_reduce(15000) // kept for a while then moved to lev2
-        , every_lev2_reduce(25000) // cleared regularly
-        , must_touch_lev1_within(40000)
-        #endif
 
         , max_temp_lev2_learnt_clauses(30000) //only used if every_lev2_reduce==0
         , inc_max_temp_lev2_red_cls(1.0)      //only used if every_lev2_reduce==0
         , protect_cl_if_improved_glue_below_this_glue_for_one_turn(30)
-        #ifndef USE_GAUSS
         , glue_put_lev0_if_below_or_eq(4) // never removed
         , glue_put_lev1_if_below_or_eq(7) // kept for a while then moved to lev2
-        #else
-        , glue_put_lev0_if_below_or_eq(6) // never removed
-        , glue_put_lev1_if_below_or_eq(11) // kept for a while then moved to lev2
-        #endif
-
 
         , clause_decay(0.999)
         , adjust_glue_if_too_many_low(0.7)
