f5c0c69572f9a86a1ac9858e8d1609da60dbd1bb
Mate Soos <soos.mate@gmail.com>
2018-05-24 21:39:03 +0800

    Use likely/unlikely for speed in propagation

--- src/constants.h
+++ src/constants.h
@@ -101,6 +101,14 @@ THE SOFTWARE.
 #endif
 
 
+#ifdef __GNUC__
+    #define likely(x) __builtin_expect((x), 1)
+    #define unlikely(x) __builtin_expect((x), 0)
+#else
+    #define likely(x) x
+    #define unlikely(x) x
+#endif
+
 ///////////////////
 // Silent Debug
 ///////////////////
