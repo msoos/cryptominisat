6c4013aaa8c514fdddbc87dfc809b184df989d0d
Mate Soos <soos.mate@gmail.com>
2018-09-26 00:53:33 +0200

    Fixing predict test script

--- scripts/learn/predict-test.sh
+++ scripts/learn/predict-test.sh
@@ -3,16 +3,22 @@ set -x
 set -e
 
 FNAME="barman-pfile07-027.sas.cr.37.cnf"
+FNAMEOUT="mystuff"
+RATIO="1"
+FIXED="50000"
+
 FNAME="UTI-20-10p0.cnf-unz"
 FNAMEOUT="mystuff2"
 RATIO="0.05"
-FIXED="80000"
+FIXED="70000"
 
+./build_stats.sh
 rm -f "$FNAMEOUT.db"
+rm -f "$FNAMEOUT.lemmas-*"
 ./cryptominisat5 --cldatadumpratio "$RATIO" --clid --sql 2 --sqlitedb "$FNAMEOUT.db" --drat "$FNAMEOUT.drat" --zero-exit-status "$FNAME"
 ./tests/drat-trim/drat-trim "$FNAME" "$FNAMEOUT.drat" -x $FNAMEOUT.lemmas -i
 ./add_lemma_ind.py "$FNAMEOUT.db" "$FNAMEOUT.lemmas"
-./gen_pandas.py "$FNAMEOUT.db" --fixed "$LIMIT"
+./gen_pandas.py "$FNAMEOUT.db" --fixed "$FIXED"
 
 ./predict.py "$FNAMEOUT.db-pandasdata.dat" --final --conf --tree --code ../src/final_predictor.cpp
 ./build_final_predictor.sh
