1f3e33d39daa6e2c9b826680d4af292d81b8b7fe
Mate Soos <soos.mate@gmail.com>
2018-10-05 00:30:40 +0200

    Feature dump_no is now special and should not be included
    This is because we mostly put into the system BAD clauses that have
    dump_no < 2

--- scripts/learn/predict.py
+++ scripts/learn/predict.py
@@ -426,7 +426,7 @@ def learn(fname):
 
         best_features = ['cl.glue_rel', 'cl.backtrack_level_hist_lt', 'rdb0.used_for_uip_creation', 'cl.size_rel', 'cl.overlap_rel', 'cl.glue_rel_long']
 
-        best_features = ['rdb0.used_for_uip_creation', 'rdb1.used_for_uip_creation', 'cl.size', 'cl.glue_rel_long', 'cl.glue_rel', 'cl.glue', 'cl.size', 'rdb0.dump_no'] #, 'cl.overlap_rel', 'cl.overlap']
+        best_features = ['rdb0.used_for_uip_creation', 'rdb1.used_for_uip_creation', 'cl.size', 'cl.glue_rel_long', 'cl.glue_rel', 'cl.glue', 'cl.size'] #, 'cl.overlap_rel', 'cl.overlap']
         best_features.append('rdb0.act_ranking_top_10')
         best_features.append('rdb0.act_ranking')
         #best_features = ['rdb0.used_for_uip_creation']
