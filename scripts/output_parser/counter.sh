b708d107de09d47cfe4cf6120ded8ab66ba16464
Mate Soos <soos.mate@gmail.com>
2018-11-19 23:03:49 +0100

    No need for this crude counter script

--- scripts/output_parser/counter.sh
+++
@@ -1,3 +0,0 @@
-#wc -l */solved | sort -n | awk 'BEGIN {ORS="\t"} {print $1 " " $2; split($2,a,"/"); x="grep \"Command being\" " a[1] "/mp1-23.3.cnf.gz.timeout"; system(x)}'
-wc -l */solved | sort -n | awk 'BEGIN {ORS="\t"} {print $1 " " $2; split($2,a,"/"); x="grep \"Command being\" " a[1] "/mp1-23.3.cnf.gz.timeout"; system(x)}' | grep 6727387 | sort -n
-
