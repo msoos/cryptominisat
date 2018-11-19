be15af7e97bbb9029bd920d0be517d49dfafdbfd
Mate Soos <soos.mate@gmail.com>
2018-11-19 22:49:31 +0100

    Better solvetime scripts

--- scripts/output_parser/solvetimes_from_output.sh
+++ scripts/output_parser/solvetimes_from_output.sh
@@ -1,28 +1,25 @@
 #!/bin/sh
 
-#set -x
+zgrep --color -i "assert.*fail" `ls *.out.gz`
+zgrep --color -i signal `ls *.out.gz`
+zgrep --color -i error `ls *.out.gz`
+zgrep --color -i terminate `ls *.out.gz`
+zgrep "signal 4" `ls *.timeout.gz`
 
-grep --color -i "assert.*fail" `ls *.out`
-grep --color -i signal `ls *.out`
-grep --color -i error `ls *.out`
-grep --color -i terminate `ls *.out`
-grep --color -i abort `ls *.out` | grep -v expensive
-grep --color -i failed `ls *.out` | grep -v probes
-
-ls -- *out | sed 's/gz.*/gz/' > allFiles
+ls -- *.out.gz | sed 's/gz.*/gz/' > allFiles
 
 # 1500 cutoff
-grep "Total" *out | awk '{print $5}' > solveTimes
-grep "Total" *out | awk '{if ($5 < 1500) {print $1}}' | sed 's/:c.*$//' | sort > solved_under_1500_full_list
+zgrep "Total" *.out.gz | awk '{print $5}' > solveTimes
+zgrep "Total" *.out.gz | awk '{if ($5 < 1500) {print $1}}' | sed 's/:c.*$//' | sort > solved_under_1500_full_list
 
 
 # for normal
-grep "s.*SATISFIABLE" *out | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved
-grep "s UNSATISFIABLE" *out | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT
-grep "s SATISFIABLE" *out   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT
+zgrep "s.*SATISFIABLE" *.out.gz | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved
+zgrep "s UNSATISFIABLE" *.out.gz | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT
+zgrep "s SATISFIABLE" *.out.gz   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT
 
 # 1500 cutoff
-grep "s.*SATISFIABLE" $(cat solved_under_1500_full_list) | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved1500
-grep "s UNSATISFIABLE" $(cat solved_under_1500_full_list) | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT1500
-grep "s SATISFIABLE" $(cat solved_under_1500_full_list)   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT1500
+zgrep "s.*SATISFIABLE" $(cat solved_under_1500_full_list) | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solved1500
+zgrep "s UNSATISFIABLE" $(cat solved_under_1500_full_list) | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT1500
+zgrep "s SATISFIABLE" $(cat solved_under_1500_full_list)   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT1500
 rm solved_under_1500_full_list
