2ccd2bbb466a8c19a97d0450a85318af5c4bff18
Mate Soos <soos.mate@gmail.com>
2018-11-22 00:25:53 +0100

    Moving to xz for compression, it produces smaller output

--- scripts/output_parser/solvetimes_from_output.sh
+++ scripts/output_parser/solvetimes_from_output.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 
 echo "checking for assert fail"
-zgrep --color -i -e "assert.*fail" -e "signal" -e "error" -e "terminate" `ls *.out.gz` > issues
+xzgrep --color -i -e "assert.*fail" -e "signal" -e "error" -e "terminate" `ls *.out.xz` > issues
 echo "checking for signal 4"
-zgrep "signal 4"  issues
+xzgrep "signal 4"  issues
 
-ls -- *.out.gz | sed 's/gz.*/gz/' > allFiles
+ls -- *.out.xz | sed 's/xz.*/xz/' > allFiles
 
 # 1500 cutoff
 echo "Getting solveTimes"
-zgrep "Total" *.out.gz | awk '{print $5}' > solveTimes
+xzgrep "Total" *.out.xz | awk '{print $5}' > solveTimes
 echo "Getting problems solved under 1500"
-zgrep "Total" *.out.gz | awk '{if ($7 < 1500) {print $1}}' | sed 's/:c.*$//' | sort > solved_under_1500_full_list
+xzgrep "Total" *.out.xz | awk '{if ($7 < 1500) {print $1}}' | sed 's/:c.*$//' | sort > solved_under_1500_full_list
 
 
 # for normal
 echo "Getting UNSAT"
-zgrep "^s UNSATISFIABLE" *.out.gz | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT
+xzgrep "^s UNSATISFIABLE" *.out.xz | sed 's/:s.*$//' | sed 's/xz.*/xz/' | sort > solvedUNSAT
 echo "Getting SAT"
-zgrep "^s SATISFIABLE" *.out.gz   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT
+xzgrep "^s SATISFIABLE" *.out.xz   | sed 's/:s.*$//' | sed 's/xz.*/xz/' | sort > solvedSAT
 rm -f solved
 cat solvedUNSAT > solved
 cat solvedSAT >> solved
 
 # 1500 cutoff
 echo "Getting 1500 UNSAT"
-zgrep "^s UNSATISFIABLE" $(cat solved_under_1500_full_list) | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedUNSAT1500
+xzgrep "^s UNSATISFIABLE" $(cat solved_under_1500_full_list) | sed 's/:s.*$//' | sed 's/xz.*/xz/' | sort > solvedUNSAT1500
 echo "Getting 1500 SAT"
-zgrep "^s SATISFIABLE" $(cat solved_under_1500_full_list)   | sed 's/:s.*$//' | sed 's/gz.*/gz/' | sort > solvedSAT1500
+xzgrep "^s SATISFIABLE" $(cat solved_under_1500_full_list)   | sed 's/:s.*$//' | sed 's/xz.*/xz/' | sort > solvedSAT1500
 rm -f solved1500
 cat solvedUNSAT1500 > solved1500
 cat solvedSAT1500 >> solved1500
