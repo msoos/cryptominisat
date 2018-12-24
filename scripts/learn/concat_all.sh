595b9dfb03964eb44cce73534624a320fe259f16
Mate Soos <soos.mate@gmail.com>
2018-12-24 13:18:53 +0100

    Fixing up concat_all

--- scripts/learn/concat_all.sh
+++ scripts/learn/concat_all.sh
@@ -32,7 +32,7 @@ done
 # run all threads
 for (( i = 0; i < $numthreads; i++))
 do
-    ./gen_pandas.py `cat files${i}` --confs 7 &
+    ./gen_pandas.py `cat files${i}` --confs 7 > "gen_pandas_${i}" &
 done
 
 # wait all threads
@@ -42,6 +42,13 @@ echo $job
     wait $job || let "FAIL+=1"
 done
 
+# check thread output
+for (( i = 0; i < $numthreads; i++))
+do
+    egrep xzgrep --color -i -e "assert.*fail" -e "signal" -e "error" -e "kill" -e "terminate" "gen_pandas_${i}"
+done
+
+
 if [ "$FAIL" == "0" ];
 then
     echo "All went well!"
@@ -50,15 +57,13 @@ else
     exit -1
 fi
 
-
-
 rm final/short-conf-${CONF}.dat
 rm final/long-conf-${CONF}.dat
 
 for CONF in {0..6}
 do
-    ./combine_dats.py -o final/short-conf-${CONF}.dat data-sing/*-short-conf-${CONF}.dat --fixed 2000
-    ./combine_dats.py -o final/long-conf-${CONF}.dat  data-sing/*-long-conf-${CONF}.dat  --fixed 2000
+    ./combine_dats.py -o final/short-conf-${CONF}.dat data-sing/*-short-conf-${CONF}.dat --fixed 2000 > "combine_out_short_${CONF}"
+    ./combine_dats.py -o final/long-conf-${CONF}.dat  data-sing/*-long-conf-${CONF}.dat  --fixed 2000 > "combine_out_long_${CONF}"
 done
 
 mkdir ../src/predict
