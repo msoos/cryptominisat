b965645c7c73154637fe12e6ce18ca0c4bf8636d
Mate Soos <soos.mate@gmail.com>
2018-12-25 23:08:40 +0100

    Fixing script to get timings

--- scripts/output_parser/solved_with_options.sh
+++ scripts/output_parser/solved_with_options.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 check="7846479"
-wc -l *${check}*/solved | sort -n | awk 'BEGIN {ORS="\t"} {print $1 " " $2; split($2,a,"/"); x="zgrep \"Command being\" " a[1] "/mp1-23.3.cnf.gz.timeout.gz"; system(x)}' | sort -n
+wc -l *${check}*/solved | sort -n | awk 'BEGIN {ORS="\t"} {print $1 " " $2; split($2,a,"/"); x="xzgrep \"Command being\" " a[1] "/mp1-23.3.cnf.gz.timeout.xz"; system(x)}' | sort -n
 wc -l *${check}*/solvedUNSAT | sort -n
 wc -l *${check}*/solvedSAT | sort -n
 
