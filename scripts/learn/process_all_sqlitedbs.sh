b71ca51754b8994fb88cd374eebfe403ee539b09
Mate Soos <soos.mate@gmail.com>
2018-12-31 22:32:56 +0100

    New run on cluster

--- scripts/learn/process_all_sqlitedbs.sh
+++ scripts/learn/process_all_sqlitedbs.sh
@@ -9,15 +9,28 @@ location="out-dats-8074972.wlm01"
 # Combine
 ###############
 
-
 rm -f out_combine_short_*
-rm -f out_combine_long_*
+rm -f comb-short-conf-*.dat
 for (( CONF = 0; CONF < numconfs; CONF++))
 do
     rm -f "comb-short-conf-${CONF}.dat"
-    rm -f "comb-long-conf-${CONF}.dat"
 
     ./combine_dats.py -o "comb-short-conf-${CONF}.dat" ${location}/*-short-conf-${CONF}.dat > "out_combine_short_${CONF}" 2>&1 &
+done
+
+# wait all threads
+FAIL=0
+for job in $(jobs -p)
+do
+    echo "waiting for job '$job' ..."
+    wait "${job}" || FAIL=$((FAIL+=1))
+done
+
+rm -f out_combine_long_*
+for (( CONF = 0; CONF < numconfs; CONF++))
+do
+    rm -f "comb-long-conf-${CONF}.dat"
+
     ./combine_dats.py -o "comb-long-conf-${CONF}.dat"  ${location}/*-long-conf-${CONF}.dat  > "out_combine_long_${CONF}" 2>&1 &
 done
 
@@ -63,10 +76,28 @@ mkdir -p ../src/predict
 rm -f ../src/predict/*.h
 rm -f out_pred_short_*
 rm -f out_pred_long_*
-for (( CONF = 0; CONF < numconfs; CONF++))
-do
-    ./predict.py "comb-long-conf-${CONF}.dat" --name long  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 9 --conf "${CONF}" --clustmin 0.03 > "out_pred_long_${CONF}" 2>&1 &
-done
+
+# no long
+./predict.py "comb-long-conf-3.dat" --name long  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 9 --conf "0" --clustmin 0.03 > "out_pred_long_0" 2>&1 &
+
+#no long, lock glue 2
+./predict.py "comb-long-conf-3.dat" --name long  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 9 --conf "1" --clustmin 0.03 > "out_pred_long_1" 2>&1 &
+
+#no long, lock glue 3
+./predict.py "comb-long-conf-3.dat" --name long  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 9 --conf "2" --clustmin 0.03 > "out_pred_long_2" 2>&1 &
+
+# 1 cluster
+./predict.py "comb-long-conf-3.dat" --name long  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 1 --conf "3" --clustmin 0.03 > "out_pred_long_3" 2>&1 &
+
+# 3 clusters
+./predict.py "comb-long-conf-3.dat" --name long  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 3 --conf "4" --clustmin 0.03 > "out_pred_long_4" 2>&1 &
+
+# 5 clusters
+./predict.py "comb-long-conf-3.dat" --name long  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 5 --conf "5" --clustmin 0.03 > "out_pred_long_5" 2>&1 &
+
+# 20 clusters, min 0.2%
+./predict.py "comb-long-conf-3.dat" --name long  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 20 --conf "6" --clustmin 0.02 > "out_pred_long_6" 2>&1 &
+
 
 # wait all threads
 FAIL=0
@@ -85,10 +116,26 @@ else
     exit 255
 fi
 
-for (( CONF = 0; CONF < numconfs; CONF++))
-do
-    ./predict.py "comb-short-conf-${CONF}.dat" --name short --basedir "../src/predict/" --final --forest --split 0.02 --clusters 9 --conf "${CONF}" --clustmin 0.03 > "out_pred_short_${CONF}" 2>&1 &
-done
+# no long
+./predict.py "comb-short-conf-3.dat" --name short  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 9 --conf "0" --clustmin 0.03 > "out_pred_short_0" 2>&1 &
+
+#no long, lock glue 2
+./predict.py "comb-short-conf-3.dat" --name short  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 9 --conf "1" --clustmin 0.03 > "out_pred_short_1" 2>&1 &
+
+#no long, lock glue 3
+./predict.py "comb-short-conf-3.dat" --name short  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 9 --conf "2" --clustmin 0.03 > "out_pred_short_2" 2>&1 &
+
+# 1 cluster
+./predict.py "comb-short-conf-3.dat" --name short  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 1 --conf "3" --clustmin 0.03 > "out_pred_short_3" 2>&1 &
+
+# 3 clusters
+./predict.py "comb-short-conf-3.dat" --name short  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 3 --conf "4" --clustmin 0.03 > "out_pred_short_4" 2>&1 &
+
+# 5 clusters
+./predict.py "comb-short-conf-3.dat" --name short  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 5 --conf "5" --clustmin 0.03 > "out_pred_short_5" 2>&1 &
+
+# 20 clusters, min 0.2%
+./predict.py "comb-short-conf-3.dat" --name short  --basedir "../src/predict/" --final --forest --split 0.02 --clusters 20 --conf "6" --clustmin 0.02 > "out_pred_short_6" 2>&1 &
 
 # wait all threads
 FAIL=0
