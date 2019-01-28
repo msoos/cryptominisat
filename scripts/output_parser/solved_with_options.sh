be6dd9f1602d8fae62be9bc20caa255750490c76
Mate Soos <soos.mate@gmail.com>
2019-01-28 22:27:43 +0100

    Fixing solved_with_options.sh

--- scripts/output_parser/solved_with_options.sh
+++ scripts/output_parser/solved_with_options.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 check="7846479"
-wc -l *${check}*/solved | sort -n | awk 'BEGIN {ORS="\t"} {print $1 " " $2; split($2,a,"/"); x="xzgrep \"Command being\" " a[1] "/mp1-23.3.cnf.gz.timeout.xz"; system(x)}' | sort -n
-wc -l *${check}*/solvedUNSAT | sort -n
-wc -l *${check}*/solvedSAT | sort -n
+wc -l *${check}*/solved.csv | sort -n | awk 'BEGIN {ORS="\t"} {print $1 " " $2; split($2,a,"/"); x="xzgrep \"Command being\" " a[1] "/mp1-23.3.cnf.gz.timeout.xz"; system(x)}' | sort -n
+wc -l *${check}*/solvedUNSAT.csv | sort -n
+wc -l *${check}*/solvedSAT.csv | sort -n
 
 echo "On SATCOMP 17 nolimits problems:"
 rm tmp
 for f in $(ls | grep ${check}); do
-	g=$(grep -f out-new-${check}.wlm01-5/allFiles $f/solved | wc -l)
+	g=$(grep -f out-new-${check}.wlm01-5/allFiles.csv $f/solved.csv | wc -l)
 	echo "$g for $f" >> tmp
 done
 sort -n tmp
