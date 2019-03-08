a3da25116cb8c65d8726ba2ce674ab47b9139dc2
Mate Soos <soos.mate@gmail.com>
2019-03-08 16:10:47 +0100

    Easier timing on the cluster

--- scripts/output_parser/create_solvetimes.sh
+++ scripts/output_parser/create_solvetimes.sh
@@ -3,7 +3,7 @@
 module unload gcc/4.9.3
 module load anaconda/3
 module load openmpi/intel/1.10.2
-for x in `ls | grep 7846479`; do
+for x in `ls | grep $1`; do
     (cd $x
     echo At $x
     pwd
--- scripts/output_parser/solved_with_options.sh
+++ scripts/output_parser/solved_with_options.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-check="7846479"
+check="$1"
 wc -l *${check}*/solved.csv | sort -n | awk 'BEGIN {ORS="\t"} {print $1 " " $2; split($2,a,"/"); x="xzgrep \"Command being\" " a[1] "/mp1-23.3.cnf.gz.timeout.xz"; system(x)}' | sort -n
 wc -l *${check}*/solvedUNSAT.csv | sort -n
 wc -l *${check}*/solvedSAT.csv | sort -n
