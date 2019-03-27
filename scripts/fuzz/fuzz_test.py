86b7f13253296e19fba57d6423d820450ce0fcbb
Mate Soos <soos.mate@gmail.com>
2019-03-27 18:55:29 +0100

    Fuzz --bvaeveryn

--- scripts/fuzz/fuzz_test.py
+++ scripts/fuzz/fuzz_test.py
@@ -324,6 +324,7 @@ class Tester:
         cmd += "--mustrenumber %d " % random.choice([0, 1])
         cmd += "--conftochrono %d " % random.choice([-1, random.randint(1, 20000)])
         cmd += "--diffdeclevelchrono %d " % random.choice([1, random.randint(1, 1000), -1])
+        cmd += "--bvaeveryn %d " % random.choice([1, random.randint(1, 20)])
 
         if self.dump_red is not None:
             cmd += "--dumpred %s " % self.dump_red
