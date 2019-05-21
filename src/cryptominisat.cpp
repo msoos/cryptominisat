1e32a5c1f7e951bdc17a7a374d9a3cb3ba12fd9d
Mate Soos <soos.mate@gmail.com>
2019-05-21 23:15:06 +0200

    Adding a few lines of code does not merit copyright over the whole work
    Removing.

--- src/cryptominisat.cpp
+++ src/cryptominisat.cpp
@@ -1,6 +1,5 @@
 /******************************************
 Copyright (c) 2016, Mate Soos
-              2019, Andrew V. Jones
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
