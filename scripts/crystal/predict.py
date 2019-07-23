585d136baadd47959be3943b23addb69addece94
Mate Soos <soos.mate@gmail.com>
2019-07-23 12:22:56 +0800

    PEP8

--- scripts/crystal/predict.py
+++ scripts/crystal/predict.py
@@ -18,6 +18,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 
+import operator
+import re
 import pandas as pd
 import pickle
 import sklearn
@@ -41,12 +43,11 @@ if int(ver[1]) < 20:
     from sklearn.cross_validation import train_test_split
 else:
     from sklearn.model_selection import train_test_split
-import re
-import operator
 
 
 def add_computed_features(df):
     print("Adding computed features...")
+
     def check_clstat_row(self, row):
         if row[self.ntoc["cl.decision_level_hist"]] == 0 or \
                 row[self.ntoc["cl.backtrack_level_hist"]] == 0 or \
@@ -62,31 +63,37 @@ def add_computed_features(df):
 
     # relative overlaps
     print("Relative overlaps...")
-    df["cl.antec_num_total_lits_rel"] = df["cl.num_total_lits_antecedents"]/df["cl.antec_sum_size_hist"]
+    df["cl.antec_num_total_lits_rel"] = df["cl.num_total_lits_antecedents"] / \
+        df["cl.antec_sum_size_hist"]
 
     # ************
     # TODO decision level and branch depth are the same, right???
     # ************
     print("size/glue/trail rel...")
-    df["(cl.trail_depth_level/cl.trail_depth_level_hist)"] = df["cl.trail_depth_level"]/df["cl.trail_depth_level_hist"]
+    df["(cl.trail_depth_level/cl.trail_depth_level_hist)"] = df["cl.trail_depth_level"] / \
+        df["cl.trail_depth_level_hist"]
 
-    df["rst_cur.all_props"] = df["rst_cur.propBinRed"] + df["rst_cur.propBinIrred"] + df["rst_cur.propLongRed"] + df["rst_cur.propLongIrred"]
-    df["(cl.num_total_lits_antecedents/cl.num_antecedents)"] = df["cl.num_total_lits_antecedents"]/df["cl.num_antecedents"]
+    df["rst_cur.all_props"] = df["rst_cur.propBinRed"] + df["rst_cur.propBinIrred"] + \
+        df["rst_cur.propLongRed"] + df["rst_cur.propLongIrred"]
+    df["(cl.num_total_lits_antecedents/cl.num_antecedents)"] = df["cl.num_total_lits_antecedents"] / \
+        df["cl.num_antecedents"]
 
     orig_cols = list(df)
     for col in orig_cols:
         if ("rdb0" in col) and "restart_type" not in col:
             col2 = col.replace("rdb0", "rdb1")
             cboth = "("+col+"+"+col2+")"
-            df[cboth]=df[col]+df[col2]
+            df[cboth] = df[col]+df[col2]
 
     df["rdb0.act_ranking_rel"] = df["rdb0.act_ranking"]/df["rdb0.tot_cls_in_db"]
     df["rdb1.act_ranking_rel"] = df["rdb1.act_ranking"]/df["rdb1.tot_cls_in_db"]
     df["((double)(rdb0.act_ranking_rel+rdb1.act_ranking_rel)/2.0)"] = \
         (df["rdb0.act_ranking_rel"]+df["rdb1.act_ranking_rel"])/2
 
-    df["rdb0.sum_uip1_used_per_tot_confl"]=df["rdb0.sum_uip1_used"]/df["cl.time_inside_solver"]
-    df["rdb1.sum_uip1_used_per_tot_confl"]=df["rdb1.sum_uip1_used"]/df["cl.time_inside_solver"]
+    df["rdb0.sum_uip1_used_per_tot_confl"] = df["rdb0.sum_uip1_used"] / \
+        df["cl.time_inside_solver"]
+    df["rdb1.sum_uip1_used_per_tot_confl"] = df["rdb1.sum_uip1_used"] / \
+        df["cl.time_inside_solver"]
 
     todiv = [
         "cl.size_hist"
@@ -143,7 +150,7 @@ def add_computed_features(df):
     for col in cols:
         if ("rdb" in col or "cl." in col or "rst" in col) and "restart_type" not in col:
             for divper in todiv:
-                df["("+col+"_<_"+divper+")"] = (df[col]<df[divper]).astype(int)
+                df["("+col+"_<_"+divper+")"] = (df[col] < df[divper]).astype(int)
                 pass
 
     # satzilla stuff
@@ -154,13 +161,14 @@ def add_computed_features(df):
         "szfeat_cur.avg_confl_size",
         "szfeat_cur.avg_branch_depth",
         "szfeat_cur.red_glue_distr_mean"
-        ]
+    ]
     for col in orig_cols:
         if "szfeat" in col:
             for divper in todiv:
                 if "min" not in todiv and "max" not in todiv:
                     df["("+col+"/"+divper+")"] = df[col]/df[divper]
-                    df["("+col+"<"+divper+")"] = (df[col]<df[divper]).astype(int)
+                    df["("+col+"<"+divper+")"] = (df[col]
+                                                  < df[divper]).astype(int)
                     pass
 
     # relative RDB
@@ -171,20 +179,22 @@ def add_computed_features(df):
             rdb1 = col.replace("rdb0", "rdb1")
             name_per = col.replace("rdb0", "rdb0_per_rdb1")
             name_larger = col.replace("rdb0", "rdb0_larger_rdb1")
-            df[name_larger]=(df[rdb0]>df[rdb1]).astype(int)
+            df[name_larger] = (df[rdb0] > df[rdb1]).astype(int)
 
             raw_col = col.replace("rdb0.", "")
             if raw_col not in ["propagations_made", "dump_no", "conflicts_made", "used_for_uip_creation", "sum_uip1_used", "clause_looked_at", "sum_delta_confl_uip1_used", "activity_rel", "last_touched_diff", "ttl"]:
                 print(rdb0)
-                df[name_per]=df[rdb0]/df[rdb1]
+                df[name_per] = df[rdb0]/df[rdb1]
 
     # smaller-or-greater comparisons
     print("smaller-or-greater comparisons...")
-    df["cl.antec_sum_size_smaller_than_hist"] = (df["cl.antec_sum_size_hist"] < df["cl.num_total_lits_antecedents"]).astype(int)
+    df["cl.antec_sum_size_smaller_than_hist"] = (
+        df["cl.antec_sum_size_hist"] < df["cl.num_total_lits_antecedents"]).astype(int)
 
-    df["cl.antec_overlap_smaller_than_hist"] = (df["cl.antec_overlap_hist"] < df["cl.num_overlap_literals"]).astype(int)
+    df["cl.antec_overlap_smaller_than_hist"] = (
+        df["cl.antec_overlap_hist"] < df["cl.num_overlap_literals"]).astype(int)
 
-    #print("flatten/list...")
+    # print("flatten/list...")
     #old = set(df.columns.values.flatten().tolist())
     #df = df.dropna(how="all")
     #new = set(df.columns.values.flatten().tolist())
@@ -200,13 +210,13 @@ def check_long_short():
         assert False
         exit(-1)
 
+
 class Learner:
     def __init__(self, df, funcname, fname, cluster_no):
         self.df = df
         self.funcname = funcname
         self.fname = fname
         self.cluster_no = cluster_no
-
 
     def output_to_classical_dot(self, clf, features):
         fname = options.dot + "-" + self.funcname
@@ -221,12 +231,13 @@ class Learner:
         print("gwenview {fname}.png".format(fname=fname))
 
     def filter_percentile(self, df, features, perc):
-        low = df.quantile(perc , axis=0)
+        low = df.quantile(perc, axis=0)
         high = df.quantile(1.0-perc, axis=0)
         df2 = df.copy()
         for i in features:
             df2 = df2[(df2[i] >= low[i]) & (df2[i] <= high[i])]
-            print("Filtered to %f on %-30s, shape now: %s" %(perc, i, df2.shape))
+            print("Filtered to %f on %-30s, shape now: %s" %
+                  (perc, i, df2.shape))
 
         print("Original size:", df.shape)
         print("New size:", df2.shape)
@@ -399,13 +410,15 @@ static bool {funcname}(
 
                 # recruse left
                 if left[node] != -1:
-                    self.recurse(left, right, threshold, features, left[node], tabs+1)
+                    self.recurse(left, right, threshold,
+                                 features, left[node], tabs+1)
 
                 self.f.write("{tabs}}} else {{\n".format(tabs=tabsize))
 
                 # recurse right
                 if right[node] != -1:
-                    self.recurse(left, right, threshold, features, right[node], tabs+1)
+                    self.recurse(left, right, threshold,
+                                 features, right[node], tabs+1)
 
                 self.f.write("{tabs}}}\n".format(tabs=tabsize))
             else:
@@ -425,9 +438,11 @@ static bool {funcname}(
             threshold = clf.tree_.threshold
             if options.verbose:
                 print("Node count:", clf.tree_.node_count)
-                print("Left: %s Right: %s Threshold: %s" % (left, right, threshold))
+                print("Left: %s Right: %s Threshold: %s" %
+                      (left, right, threshold))
                 print("clf.tree_.feature:", clf.tree_.feature)
-            features = [self.feat[i % len(self.feat)] for i in clf.tree_.feature]
+            features = [self.feat[i % len(self.feat)]
+                        for i in clf.tree_.feature]
             self.value = clf.tree_.value
 
             self.recurse(left, right, threshold, features, 0, starttab)
@@ -487,7 +502,8 @@ static bool {funcname}(
         train, test = train_test_split(df, test_size=0.33)
         X_train = train[features]
         y_train = train[to_predict]
-        split_point = helper.calc_min_split_point(df, options.min_samples_split)
+        split_point = helper.calc_min_split_point(
+            df, options.min_samples_split)
         del df
 
         t = time.time()
@@ -496,9 +512,9 @@ static bool {funcname}(
 
         if final:
             clf_tree = sklearn.tree.DecisionTreeClassifier(
-                    max_depth=options.tree_depth,
-                    class_weight={"OK": prefer_ok, "BAD": 1},
-                    min_samples_split=split_point)
+                max_depth=options.tree_depth,
+                class_weight={"OK": prefer_ok, "BAD": 1},
+                min_samples_split=split_point)
 
             clf_svm_pre = sklearn.svm.SVC(C=500, gamma=10**-5)
             clf_svm = sklearn.ensemble.BaggingClassifier(
@@ -511,10 +527,10 @@ static bool {funcname}(
                 penalty="l1")
 
             clf_forest = sklearn.ensemble.RandomForestClassifier(
-                    n_estimators=options.num_trees,
-                    max_features="sqrt",
-                    class_weight={"OK": prefer_ok, "BAD": 1},
-                    min_samples_leaf=split_point)
+                n_estimators=options.num_trees,
+                max_features="sqrt",
+                class_weight={"OK": prefer_ok, "BAD": 1},
+                min_samples_leaf=split_point)
 
             if options.final_is_tree:
                 clf = clf_tree
@@ -525,10 +541,12 @@ static bool {funcname}(
             elif options.final_is_forest:
                 clf = clf_forest
             elif options.final_is_voting:
-                mylist = [["forest", clf_forest], ["svm", clf_svm], ["logreg", clf_logreg]]
+                mylist = [["forest", clf_forest], [
+                    "svm", clf_svm], ["logreg", clf_logreg]]
                 clf = sklearn.ensemble.VotingClassifier(mylist)
             else:
-                print("ERROR: You MUST give one of: tree/forest/svm/logreg/voting classifier")
+                print(
+                    "ERROR: You MUST give one of: tree/forest/svm/logreg/voting classifier")
                 exit(-1)
         else:
             clf = sklearn.ensemble.RandomForestClassifier(
@@ -544,7 +562,8 @@ static bool {funcname}(
         best_features = []
         if not final:
             importances = clf.feature_importances_
-            std = np.std([tree.feature_importances_ for tree in clf.estimators_], axis=0)
+            std = np.std(
+                [tree.feature_importances_ for tree in clf.estimators_], axis=0)
             indices = np.argsort(importances)[::-1]
             indices = indices[:options.top_num_features]
             myrange = min(X_train.shape[1], options.top_num_features)
@@ -564,7 +583,8 @@ static bool {funcname}(
                 plt.bar(range(myrange), importances[indices],
                         color="r", align="center",
                         yerr=std[indices])
-                plt.xticks(range(myrange), [features[x] for x in indices], rotation=45)
+                plt.xticks(range(myrange), [features[x]
+                                            for x in indices], rotation=45)
                 plt.xlim([-1, myrange])
 
         if options.dot is not None and final:
@@ -611,7 +631,7 @@ static bool {funcname}(
         print("- min avg dumpno: %1.3f -" % options.min_avg_dumpno)
         print("-------------------------")
         self.filtered_conf_matrixes(
-                dump_no, train, features, to_predict, clf, "train")
+            dump_no, train, features, to_predict, clf, "train")
 
         # TODO do L1 regularization
 
@@ -674,75 +694,76 @@ static bool {funcname}(
         if options.show:
             plt.show()
 
+
 if __name__ == "__main__":
     usage = "usage: %(prog)s [options] file.pandas"
     parser = argparse.ArgumentParser(usage=usage)
 
     parser.add_argument("fname", type=str, metavar='PANDASFILE')
     parser.add_argument("--verbose", "-v", action="store_true", default=False,
-                      dest="verbose", help="Print more output")
+                        dest="verbose", help="Print more output")
     parser.add_argument("--printfeat", action="store_true", default=False,
-                      dest="print_features", help="Print features")
+                        dest="print_features", help="Print features")
     parser.add_argument("--nocomputed", default=False, action="store_true",
-                      dest="no_computed", help="Don't add computed features")
+                        dest="no_computed", help="Don't add computed features")
 
     # tree/forest options
     parser.add_argument("--depth", default=None, type=int,
-                      dest="tree_depth", help="Depth of the tree to create")
+                        dest="tree_depth", help="Depth of the tree to create")
     parser.add_argument("--split", default=0.01, type=float, metavar="RATIO",
-                      dest="min_samples_split", help="Split in tree if this many samples or above. Used as a percentage of datapoints")
+                        dest="min_samples_split", help="Split in tree if this many samples or above. Used as a percentage of datapoints")
     parser.add_argument("--numtrees", default=5, type=int,
-                      dest="num_trees", help="How many trees to generate for the forest")
+                        dest="num_trees", help="How many trees to generate for the forest")
 
     # generation of predictor
     parser.add_argument("--dot", type=str, default=None,
-                      dest="dot", help="Create DOT file")
+                        dest="dot", help="Create DOT file")
     parser.add_argument("--filterdot", default=0.05, type=float,
-                      dest="filter_dot", help="Filter the DOT output from outliers so the graph looks nicer")
+                        dest="filter_dot", help="Filter the DOT output from outliers so the graph looks nicer")
     parser.add_argument("--show", action="store_true", default=False,
-                      dest="show", help="Show visual graphs")
+                        dest="show", help="Show visual graphs")
     parser.add_argument("--check", action="store_true", default=False,
-                      dest="check_row_data", help="Check row data for NaN or float overflow")
+                        dest="check_row_data", help="Check row data for NaN or float overflow")
     parser.add_argument("--rawplots", action="store_true", default=False,
-                      dest="raw_data_plots", help="Display raw data plots")
+                        dest="raw_data_plots", help="Display raw data plots")
     parser.add_argument("--basedir", type=str,
-                      dest="basedir", help="The base directory of where the CryptoMiniSat source code is")
+                        dest="basedir", help="The base directory of where the CryptoMiniSat source code is")
     parser.add_argument("--conf", default=0, type=int,
-                      dest="conf_num", help="Which predict configuration this is")
+                        dest="conf_num", help="Which predict configuration this is")
 
     # data filtering
     parser.add_argument("--only", default=0.99, type=float,
-                      dest="only_pecr", help="Only use this percentage of data")
+                        dest="only_pecr", help="Only use this percentage of data")
 
     # final generator or greedy
     parser.add_argument("--final", default=False, action="store_true",
-                      dest="only_final", help="Only generate final predictor")
+                        dest="only_final", help="Only generate final predictor")
     parser.add_argument("--greedy", default=None, type=int, metavar="TOPN",
-                      dest="get_best_topn_feats", help="Greedy Best K top features from the top N features given by '--top N'")
+                        dest="get_best_topn_feats", help="Greedy Best K top features from the top N features given by '--top N'")
     parser.add_argument("--top", default=None, type=int, metavar="TOPN",
-                      dest="top_num_features", help="Candidates are top N features for greedy selector")
+                        dest="top_num_features", help="Candidates are top N features for greedy selector")
 
     # type of classifier
     parser.add_argument("--tree", default=False, action="store_true",
-                      dest="final_is_tree", help="Final classifier should be a tree")
+                        dest="final_is_tree", help="Final classifier should be a tree")
     parser.add_argument("--svm", default=False, action="store_true",
-                      dest="final_is_svm", help="Final classifier should be an svm")
+                        dest="final_is_svm", help="Final classifier should be an svm")
     parser.add_argument("--lreg", default=False, action="store_true",
-                      dest="final_is_logreg", help="Final classifier should be a logistic regression")
+                        dest="final_is_logreg", help="Final classifier should be a logistic regression")
     parser.add_argument("--forest", default=False, action="store_true",
-                      dest="final_is_forest", help="Final classifier should be a forest")
+                        dest="final_is_forest", help="Final classifier should be a forest")
     parser.add_argument("--voting", default=False, action="store_true",
-                      dest="final_is_voting", help="Final classifier should be a voting of all of: forest, svm, logreg")
+                        dest="final_is_voting", help="Final classifier should be a voting of all of: forest, svm, logreg")
 
     # classifier options
     parser.add_argument("--prefok", default=2.0, type=float,
-                      dest="prefer_ok", help="Prefer OK if >1.0, equal weight if = 1.0, prefer BAD if < 1.0")
+                        dest="prefer_ok", help="Prefer OK if >1.0, equal weight if = 1.0, prefer BAD if < 1.0")
 
     # which one to generate
     parser.add_argument("--name", default=None, type=str,
-                      dest="longsh", help="Raw C-like code will be written to this function and file name")
+                        dest="longsh", help="Raw C-like code will be written to this function and file name")
     parser.add_argument("--clust", default=False, action="store_true",
-                      dest="use_clusters", help="Use clusters")
+                        dest="use_clusters", help="Use clusters")
 
     options = parser.parse_args()
 
@@ -778,7 +799,7 @@ if __name__ == "__main__":
     if options.use_clusters:
         used_clusters = df.groupby("clust").nunique()
         clusters = []
-        for clust,_ in used_clusters["clust"].iteritems():
+        for clust, _ in used_clusters["clust"].iteritems():
             clusters.append(clust)
     else:
         clusters = [0]
@@ -801,7 +822,7 @@ if __name__ == "__main__":
             df[(df["clust"] == clno)],
             funcname=funcname,
             fname=f,
-            cluster_no = clno)
+            cluster_no=clno)
 
         print("================ Cluster %3d ================" % clno)
         learner.learn()
