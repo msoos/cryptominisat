b35cae72ca6ef006ccec4458f116d1fc3c65c059
Mate Soos <soos.mate@gmail.com>
2019-08-08 17:01:02 +0800

    Change order of branch strategies

--- src/solver.cpp
+++ src/solver.cpp
@@ -1706,13 +1706,13 @@ long Solver::calc_num_confl_to_do_this_iter(const size_t iteration_num) const
 
 void Solver::set_branch_strategy(const uint32_t iteration_num)
 {
-    long modulo = iteration_num % 4+2;
+    long modulo = iteration_num % 4;
     if (modulo == 0) {
-        branch_strategy = branch::vsids;
-    } else if (modulo == 1) {
-        branch_strategy = branch::maple;
-    } else if (modulo == 2) {
         branch_strategy = branch::vmtf;
+    } else if (modulo == 1) {
+        branch_strategy = branch::vsids;
+    } else if (modulo == 2) {
+        branch_strategy = branch::maple;
     } else if (modulo == 3) {
         branch_strategy = branch::rnd;
     }
