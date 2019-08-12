fe94c8df4bb0c81753b0a478578ed15f6257aeee
Mate Soos <soos.mate@gmail.com>
2019-08-12 18:37:54 +0800

    Save polarity from yalsat to CMS

--- src/yalsat.cpp
+++ src/yalsat.cpp
@@ -208,7 +208,13 @@ lbool Yalsat::deal_with_solution(int res)
     if (res != 10) {
         if (solver->conf.verbosity) {
             cout << "c [yalsat] ASSIGNMENT NOT FOUND" << endl;
+            cout << "c [yalsat] best assignement had " << yals_minimum(yals) << " clauses unsatisfied" << endl;
         }
+
+        for(size_t i = 0; i < solver->nVars(); i++) {
+            solver->varData[i].polarity = (yals_deref(yals, i+1) >= 0);
+        }
+
         return l_Undef;
     }
 
