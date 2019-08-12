4e300d09d3db1a95eea028a23a68f2e3c1af0837
Mate Soos <soos.mate@gmail.com>
2019-08-12 18:58:41 +0800

    Make getting of phase from SLS optional

--- src/yalsat.cpp
+++ src/yalsat.cpp
@@ -211,8 +211,10 @@ lbool Yalsat::deal_with_solution(int res)
             cout << "c [yalsat] best assignement had " << yals_minimum(yals) << " clauses unsatisfied" << endl;
         }
 
-        for(size_t i = 0; i < solver->nVars(); i++) {
-            solver->varData[i].polarity = (yals_deref(yals, i+1) >= 0);
+        if (solver->conf.sls_get_phase) {
+            for(size_t i = 0; i < solver->nVars(); i++) {
+                solver->varData[i].polarity = (yals_deref(yals, i+1) >= 0);
+            }
         }
 
         return l_Undef;
