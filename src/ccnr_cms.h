b354347871066f403f003b69fd58f3511edd7ef5
Mate Soos <soos.mate@gmail.com>
2019-08-16 14:37:00 +0800

    Minor cleanup

--- src/ccnr_cms.h
+++ src/ccnr_cms.h
@@ -55,7 +55,7 @@ private:
     void init_for_round();
     bool init_problem();
     lbool deal_with_solution(int res);
-    CCNR::ls_solver* ls_s;
+    CCNR::ls_solver* ls_s = NULL;
     uint32_t cl_num = 0;
 
     enum class add_cl_ret {added_cl, skipped_cl, unsat};
