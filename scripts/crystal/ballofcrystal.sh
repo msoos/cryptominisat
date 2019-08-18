cab4f3d67d91251a3ea7c70d04e0d73c2c6bdb02
Mate Soos <soos.mate@gmail.com>
2019-08-18 20:43:48 +0800

    Let's not have too much data in ballofcrystal

--- scripts/crystal/ballofcrystal.sh
+++ scripts/crystal/ballofcrystal.sh
@@ -145,7 +145,7 @@ cp "$FNAMEOUT.db" "$FNAMEOUT-min.db"
 ########################
 ../gen_pandas.py "${FNAMEOUT}-min.db" --limit "$FIXED" --conf $CONF-$CONF ${EXTRA_GEN_PANDAS_OPTS}
 ../vardata_gen_pandas.py "${FNAMEOUT}.db" --limit 10000
-../vardata_predict.py mydata.db-vardata.dat --picktimeonly --csv -q 2
+../vardata_predict.py mydata.db-vardata.dat --picktimeonly --csv -q 2 --only 0.1
 
 ########################
 # Create the classifiers
