2a6062f3006bc5c0ce36d543831ff946ae2e46a9
Mate Soos <soos.mate@gmail.com>
2019-12-08 17:37:23 +0100

    Initial cdclt.py

--- src/cdclt.py
+++ src/cdclt.py
@@ -26,62 +26,40 @@ cards4 = [
     [[-10, -11], 1]
 ]
 
-verb = True
 print("--------- INIT FINISH ----------")
 
-# Propagates
+# Propagate or conflict based on the cardinality constraints
+# 'ass' contains the assignements:
+# -> ass[1] = 0  means variable 1 is UNDEFINED
+# -> ass[1] = 1  means variable 1 is TRUE
+# -> ass[1] = -1 means variable 1 is FALSE
 def propagate(ass):
-    if verb:
-        print("propagate() called")
-        print("variable values: ")
-        for l, i in zip(ass, range(100000)):
-            if i > 0:
-                print ("%d: %d" % (i, l))
+    print("propagate() called")
+    print("variable assignments: ")
+    for val, i in zip(ass, range(100000)):
+        if i > 0:
+            print ("%d: %d" % (i, val))
 
-    #Let's go through all the cardinality constraints and see what's up
-    for lhs, rhs in cards:
-        print("lhs: %s <= %s" % (lhs, rhs))
-        val = 0
-        reason = []
-        prop_lits = []
-        for p in lhs:
-            #assigned variable in cardinality constraint
-            #could be part of a reason!
-            if ass[abs(p)] != 0:
-                var = abs(p)
-                if p < 0:
-                    neg = -1
-                else:
-                    neg = 1
-
-                #Only put FALSIFIED values into the reason
-                if ass[var]*neg > 0:
-                    reason.append(-p)
-
-            #unassigned variable in cardinality constraint
-            if ass[abs(p)] == 0:
-                #potentially propagated literals
-                prop_lits.append(-p)
-
-            #Let's update the current value
-            if p > 0:
-                val += int(ass[p] > 0)
-            else:
-                val += int(-ass[abs(p)] > 0)
-
-        print("val   : ", val)
-        print("rhs   : ", rhs)
-        if val > rhs:
-            print("Conflict! Reason clause:", reason)
-            return 2, [], reason
-
-        if val == rhs and len(prop_lits) != 0:
-            props = []
-            for prop_lit in prop_lits:
-                prop = [prop_lit] + reason
-                print("Propagation! Reason clause:", prop)
-                props.append(prop)
-
-            return 1, props
-
+    # nothing propagated or conflicted, return 0
     return 0
+
+    # to return a conflict do:
+    #return 2, [], conflict_reason
+    # where conflict_reason is a list of literals that are all falsified
+    # e.g. [1, 2, -3] where:
+    # -> ass[1] = -1
+    # -> ass[2] = -1
+    # -> ass[3] = 1
+
+    # to return an invdividual propagation, do:
+    #return 1, [propagating_reason]
+    # where propagating_reason is a list of literals that are all falsified
+    # except the first literal which is undefined, e.g.
+    # e.g. [4, 2, -3] where:
+    # -> ass[1] = 0
+    # -> ass[2] = -1
+    # -> ass[3] = 1
+
+    # to return multiple proapgations, do:
+    #return 1, propagating_reasons
+    # where "propagating_reasons" is a list of "propagation_reason"-s
