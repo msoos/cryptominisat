e3281456e25dc5f33646a109b4d5ced7a05e0fe9
Mate Soos <soos.mate@gmail.com>
2019-12-08 09:59:41 +0100

    Better code on the python side

--- src/cdclt.py
+++ src/cdclt.py
@@ -3,24 +3,25 @@ print("--------- INIT ----------")
 pbs = [
     #v1 + v2 <= 1
     [[1, 2], 1],
-    [[1, 3], 1],
-    [[-1, -3], 1],
     [[-1, 2], 1],
-    [[1, -2], 1],
-    [[-2, -3], 1]
+    [[-1, -2], 1],
+    [[1, 3], 1],
+    [[-1, -3], 1]
     ]
+verb = False
 
 # Propagates
 def propagate(ass):
-    print("propagate() call here")
-    print("values: ")
-    for l, i in zip(ass, range(1000000)):
-        if i > 0:
-            print ("%d: %d" % (i, l))
+    if verb:
+        print("propagate() called")
+        print("variable values: ")
+        for l, i in zip(ass, range(100000)):
+            if i > 0:
+                print ("%d: %d" % (i, l))
 
     for pb, maxval in pbs:
-        print("pb  :", pb)
-        print("card:", maxval)
+        #print("pb  :", pb)
+        #print("card:", maxval)
         val = 0
         reason = []
         prop_lit = 0
@@ -29,17 +30,12 @@ def propagate(ass):
                 reason.append(-p)
 
             if ass[abs(p)] == 0:
-                prop_lit = p
+                prop_lit = -p
 
             if p > 0:
                 val += int(ass[p] > 0)
-                #print("norm inc due to:", p)
-                #print("val:", val)
             else:
                 val += int(-ass[abs(p)] > 0)
-                #print("inv  inc due to:", p)
-                #print(" -- ass[abs(p)]:", ass[abs(p)])
-                #print("val:", val)
 
         print("val   : ", val)
         print("maxval: ", maxval)
@@ -48,8 +44,8 @@ def propagate(ass):
             return 2, [], reason
 
         if val == maxval and prop_lit != 0:
-            print("prop lit:", prop_lit)
-            print("reason:", reason)
+            #print("prop lit:", prop_lit)
+            #print("reason:", reason)
             prop = [prop_lit] + reason
             print("Propagation! Reason clause:", prop)
             return 1, [prop]
