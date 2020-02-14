391252e5faf289b36e66b39897a8a9e2c48cc3d2
Mate Soos <soos.mate@gmail.com>
2020-02-15 00:56:14 +0100

    Better speed checking script

--- scripts/output_parser/solved_with_options.sh
+++ scripts/output_parser/solved_with_options.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+check="$1"
+
 rm out
 for d in `ls | grep $check`; do
     echo $d;
@@ -8,6 +10,7 @@ for d in `ls | grep $check`; do
     numsolved=`wc -l $d/solved.csv | awk '{print $1}'`
     numUNSAT=`wc -l $d/solvedUNSAT.csv | awk '{print $1}'`
     numSAT=`wc -l $d/solvedSAT.csv  | awk '{print $1}'`
-    echo "$PAR2 $d $numsolved $numSAT $numUNSAT $name" >> out
+    rev=`xzgrep -i "revision" $d/20180321_110706599_p_cnf_320_1120.cnf.gz.out.xz | awk '{print $5}' | cut -c1-7`
+    echo "$PAR2 $d $numsolved $numSAT $numUNSAT $rev $name" >> out
 done
 sed "s/20180321_110706.*//" out | sed "s/Command.*time.*cryptominisat5//"  | sed "s/-drat0.solved.csv//" |                 sed "s/ *Command being timed.*cryptominisat5//" | sed "s/\t/ /g" | sed "s/\t/ /g" | sort -n
