0c24cbbef77b49c3a179db1ec51c44bb26fc5846
Mate Soos <soos.mate@gmail.com>
2020-04-17 22:18:09 +0200

    Adding simple polar setup

--- src/searcher.cpp
+++ src/searcher.cpp
@@ -2406,9 +2406,11 @@ bool Searcher::must_abort(const lbool status) {
 
 void Searcher::setup_polarity_strategy()
 {
-    if (branch_strategy_num > 0) {
+    if (branch_strategy_num > 0 && conf.polar_stable_every_n > 0) {
         polar_stable = (branch_strategy_num % conf.polar_stable_every_n) == 0;
         polar_stable_longest_trail = 0;
+    } else if (conf.polar_stable_every_n == 0) {
+        polar_stable = true;
     }
     if (conf.verbosity) {
         cout << "c [polar] stable polarities: " << polar_stable << endl;
