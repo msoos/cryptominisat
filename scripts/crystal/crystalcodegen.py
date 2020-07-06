712ffeef25e11c4f947d1e6bd2c30e1a3739e0f6
Mate Soos <soos.mate@gmail.com>
2020-07-06 17:33:53 +0200

    Fixing copyright

--- scripts/crystal/crystalcodegen.py
+++ scripts/crystal/crystalcodegen.py
@@ -1,12 +1,23 @@
-"""
-    codegen
-    ~~~~~~~
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-    Extension to ast that allow ast -> python code generation.
+# Copyright (C) 2020  Mate Soos
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 
-    :copyright: Copyright 2008 by Armin Ronacher.
-    :license: BSD.
-"""
 import ast
 from pprint import pprint
 
