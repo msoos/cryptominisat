ecd24ac4d96c79e9b52e55ecc91d665e3aa410b3
Mate Soos <soos.mate@gmail.com>
2020-09-02 19:43:27 +0200

    Making the learn.sh more suited for multi-runs

--- scripts/crystal/learn.sh
+++ scripts/crystal/learn.sh
@@ -23,29 +23,48 @@
 #set -x
 set -e
 
-rm -f out_* classifiers/*
-git rev-parse HEAD > out_git
-cat learn.sh >> out_git
-md5sum *.dat >> out_git
+function concat() {
+    rm -f out_*
+    rm -f classifiers/*
+    git rev-parse HEAD > out_git
+    cat learn.sh >> out_git
+    md5sum *.dat >> out_git
 
-only=0.99
-bestf="../../scripts/crystal/best_features.txt"
-bestf="../../scripts/crystal/best_features.txt-ext"
-bestf="../../scripts/crystal/best_features.txt-rdb0-only"
-../cldata_predict.py short-comb-*.dat --tier short --xgboost --final --only $only \
-     --basedir ../../src/predict/ --name short --bestfeatfile $bestf | tee out_short
+    only=0.99
+    bestf="../../scripts/crystal/best_features.txt"
+    bestf="../../scripts/crystal/best_features.txt-ext"
+    bestf="../../scripts/crystal/best_features.txt-rdb0-only"
+    ../cldata_predict.py \
+        short-comb-cut1-${cut1}-cut2-${cut2}-limit-${limit}.dat \
+        --tier short --xgboost --final --only $only \
+        --basedir ../../src/predict/ --name short --bestfeatfile $bestf | tee out_short
 
-../cldata_predict.py long-comb-*.dat  --tier long --xgboost --final --only $only \
-    --basedir ../../src/predict/ --name long --bestfeatfile $bestf | tee out_long
+    ../cldata_predict.py \
+        long-comb-cut1-${cut1}-cut2-${cut2}-limit-${limit}.dat  \
+        --tier long --xgboost --final --only $only \
+        --basedir ../../src/predict/ --name long --bestfeatfile $bestf | tee out_long
 
-../cldata_predict.py forever-comb-*.dat --tier forever --xgboost --final --only $only \
-    --basedir ../../src/predict/ --name forever --topperc --bestfeatfile $bestf | tee out_forever
+    ../cldata_predict.py \
+        forever_div-comb-cut1-${cut1}-cut2-${cut2}-limit-${limit}.dat \
+        --tier forever --xgboost --final --only $only \
+        --basedir ../../src/predict/ --name forever --topperc --bestfeatfile $bestf | tee out_forever
 
-#pid3=$!
-#wait $pid1
-#wait $pid2
-#wait $pid3
-cp ../../src/predict/*.json classifiers/
-cp out_* classifiers/
+    cp ../../src/predict/*.json classifiers/
+    cp out_* classifiers/
+    tar czvf classifiers-cut1-${cut1}-cut2-${cut2}-limit-${limit}.tar.gz classifiers
+}
+
+limit=700
+cut1="10.0"
+cut2="40.0"
+concat
+
+cut1="20.0"
+cut2="50.0"
+concat
+
+cut1="40.0"
+cut2="70.0"
+concat
 
 exit 0
