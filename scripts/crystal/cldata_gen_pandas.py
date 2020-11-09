385539c15c246d9a9cd29cb01fb6606649d5b375
Mate Soos <soos.mate@gmail.com>
2020-11-09 19:43:03 +0100

    Convert types in pandas so we can do NaN

--- scripts/crystal/cldata_gen_pandas.py
+++ scripts/crystal/cldata_gen_pandas.py
@@ -385,8 +385,9 @@ def dump_dataframe(df, name):
 
     fname = "%s.dat" % name
     print("Dumping pandas data to:", fname)
+    df_float = df.convert_dtypes()
     with open(fname, "wb") as f:
-        pickle.dump(df, f)
+        pickle.dump(df_float, f)
 
 
 def one_database(dbfname):
