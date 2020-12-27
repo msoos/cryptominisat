eb86f7b2cfb92f24bbd3965c1af4a5b975108734
Mate Soos <soos.mate@gmail.com>
2020-12-27 13:49:41 +0100

    Much faster create solvetimes

--- scripts/output_parser/create_solvetimes.sh
+++ scripts/output_parser/create_solvetimes.sh
@@ -3,11 +3,23 @@
 module unload gcc/4.9.3
 module load anaconda/3
 module load openmpi/intel/1.10.2
+mypids=()
 for x in `ls | grep $1`; do
-    (cd $x
+    cd $x
     echo At $x
     pwd
     ../solvetimes_from_output.sh
-    );
+    pid=$!
+    echo "PID here is $pid"
+    mypids+=("$pid")
+    cd ..
 done
+
+echo "PIDS to wait for are: ${mypids[*]}"
+for pid2 in "${mypids[@]}"
+do
+    echo "Waiting for $pid2 ..."
+    wait $pid2
+done
+
 
