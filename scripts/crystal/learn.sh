d5ccfa9056c7109cd2ca3d5139231cb5ea0b4e4c
Mate Soos <soos.mate@gmail.com>
2020-12-29 13:20:59 +0100

    How I generated the -w3/w5/-est40/-est60 systems

--- scripts/crystal/learn.sh
+++ scripts/crystal/learn.sh
@@ -25,44 +25,65 @@ set -e
 
 function concat() {
     rm -f out_*
+    rm -f ../../src/predict/*.json
     rm -f classifiers/*
     git rev-parse HEAD > out_git
     cat learn.sh >> out_git
     md5sum *.dat >> out_git
 
     bestf="../../scripts/crystal/best_features-rdb0-only.txt"
+    name="cut1-${cut1}-cut2-${cut2}-limit-${limit}-est${est}-w${w}"
 
     tiers=("short" "long" "forever")
     for tier in "${tiers[@]}"
     do
-        /usr/bin/time --verbose -o "out_${tier}-cut1-${cut1}-cut2-${cut2}-limit-${limit}-est${est}.timeout" \
+        /usr/bin/time --verbose -o "classifiers/out_${tier}-${name}.timeout" \
         ../cldata_predict.py \
         ${tier}-comb-cut1-${cut1}-cut2-${cut2}-limit-${limit}.dat \
         --tier ${tier} --final --xgboost \
-        --xgboostest ${est} \
-        --basedir ../../src/predict/ --bestfeatfile ${bestf} | tee out_${tier}
+        --xgboostest ${est} --weight ${w} \
+        --basedir ../../src/predict/ --bestfeatfile ${bestf} | tee classifiers/out_${tier}
     done
 
     cp ../../src/predict/*.json classifiers/
-    cp out_* classifiers/
-    tar czvf classifiers-cut1-${cut1}-cut2-${cut2}-limit-${limit}-est${est}.tar.gz classifiers
+    tar czvf classifiers-${name}.tar.gz classifiers
 }
 
 limit=2000
+w=0
 # est=20
 # cut1="50.0"
 # cut2="80.0"
 # concat
 
+# w=3
+# est=20
+# cut1="10.0"
+# cut2="40.0"
+# concat
+#
+# w=5
 # est=20
 # cut1="10.0"
 # cut2="40.0"
 # concat
 
+w=0
 est=40
-cut1="40.0"
-cut2="70.0"
+cut1="10.0"
+cut2="40.0"
 concat
+
+w=0
+est=60
+cut1="10.0"
+cut2="40.0"
+concat
+
+# est=40
+# cut1="40.0"
+# cut2="70.0"
+# concat
 
 # est=20
 # cut1="40.0"
