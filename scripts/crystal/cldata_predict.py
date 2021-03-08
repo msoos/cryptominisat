89b1e01b5bf9117f4431c8b9be812a1294630387
Mate Soos <soos.mate@gmail.com>
2021-03-08 22:59:35 +0100

    Fix missing data when e.g. glue is nonsensical (ternary clauses)

--- scripts/crystal/cldata_predict.py
+++ scripts/crystal/cldata_predict.py
@@ -563,6 +563,14 @@ if __name__ == "__main__":
         del df_nofloat
     else:
         df = pd.read_pickle(options.fname)
+        filt = df[df["cl.glue_before_minim"].apply(type)==None]
+        def f(x):
+            if x is None:
+                return np.nan
+            else:
+                return x
+
+        df["cl.glue_before_minim"] = df["cl.glue_before_minim"].apply(f)
     df_orig = df.copy()
     if options.print_features:
         for f in sorted(list(df)):
