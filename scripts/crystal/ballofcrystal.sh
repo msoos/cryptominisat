1809ee5037c3d06ea24cc58c25e7031f90eb59da
Mate Soos <soos.mate@gmail.com>
2021-09-26 23:39:39 +0200

    Removing extra end-of-line

--- scripts/crystal/ballofcrystal.sh
+++ scripts/crystal/ballofcrystal.sh
@@ -218,4 +218,4 @@ exit
 # ./cryptominisat5 goldb-heqc-i10mul.cnf --simdrat 1 --printsol 0 --predloc ./data/14-april-2021-69bad529f962c-cut1-3.0-cut2-25.0-limit-1000-est10-w0-xbmin50-xbmd6/ --predtype py --predbestfeats ./best_features-b93210018231ab04d2.txt
 
 # current
-# ./cryptominisat5 goldb-heqc-i10mul.cnf --simdrat 1 --printsol 0 --predloc ./data/18-sept-2021-a408d53c665f9305b-cut1-3.0-cut2-25.0-limit-1000-est10-w0-xbmin50-xbmd6-regxgb/ --predtype py --predbestfeats ./best_features-rdb0-only.txt
+# ./cryptominisat5 goldb-heqc-i10mul.cnf --simdrat 1 --printsol 0 --predloc ./data/18-sept-2021-a408d53c665f9305b-cut1-3.0-cut2-25.0-limit-1000-est15-w0-xbmin50-xbmd4-regxgb/ --predtype py --predbestfeats ./best_features-rdb0-only.txt
