e617f8c798a2847f9cacfc58b6b2894534fde88c
Mate Soos <soos.mate@gmail.com>
2021-10-01 13:58:14 +0200

    Updating msqe script

--- scripts/crystal/get_msqe.py
+++ scripts/crystal/get_msqe.py
@@ -1,16 +1,28 @@
 #!/bin/python3
 
 import sys
+import os
 
 assert len(sys.argv) >= 2
 
-for dir in sys.argv[1:]:
-    print(dir)
+
+tiers = {"short":[], "long":[], "forever":[]}
+
+for mydir in sys.argv[1:]:
+    if not os.path.isdir(mydir):
+        print("WARN: %s is not a directory, skipping" % mydir)
+        continue
+    print(mydir)
     print("========")
     for tier in ["short", "long", "forever"]:
+        fname = "%s/out_%s" %(mydir, tier)
+        if not os.path.isfile(fname):
+            print("ERROR: the file '%s' does not exist!" % fname)
+            exit(-1)
+
         test = False
         all_dump_no = False
-        with open("%s/out_%s" %(dir, tier), "r")  as f:
+        with open(fname, "r")  as f:
             out = "Tier %-9s " % tier
             for line in f:
                 line = line.strip()
@@ -34,6 +46,15 @@ for dir in sys.argv[1:]:
     
                 assert test
                 if "Mean squared error is" in line:
+                    mydat = line.split(" ")
+                    val = float(mydat[5])
+                    tiers[tier].append([mydir, val])
                     out += line
                     print(out)
                     break
+
+for tier in ["short", "long", "forever"]:
+    print("TIER: ", tier)
+    top =  sorted(tiers[tier], key=lambda x: x[1], reverse=False)
+    for t in top:
+        print(t)
