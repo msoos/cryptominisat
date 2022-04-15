52345863fb40f37216e11de4d719db52ddf8ebc6
Mate Soos <soos.mate@gmail.com>
2022-04-15 11:14:17 +0200

    Adding initial LPN script

---
+++ scripts/gen-lpn.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2009-2020 Authors of CryptoMiniSat, see AUTHORS file
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+class PlainHelpFormatter(optparse.IndentedHelpFormatter):
+
+    def format_description(self, description):
+        if description:
+            return description + "\n"
+        else:
+            return ""
+
+
+usage = "usage: %prog [options] --fuzz/--regtest/--checkdir/filetocheck"
+desc = """Fuzz the solver with fuzz-generator: ./fuzz_test.py
+"""
+
+
+def set_up_parser():
+    parser = optparse.OptionParser(usage=usage, description=desc,
+                                   formatter=PlainHelpFormatter())
+
+    parser.add_option("--verbose", "-v", action="store_true", default=False,
+                      dest="verbose", help="Print more output")
+
+    # for fuzz-testing
+    parser.add_option("--seed", dest="seed", default=1
+                      help="Genereate with this seed", type=int)
+
+
+    parser.add_option("-n", dest="n", default=8, type=int
+                      help="Functiion width")
+    parser.add_option("--output", "-o", dest="samples", default=8, type=int
+                      help="Number of samples")
+    parser.add_option("--noise", "-k", dest="noise", default=0.1, type=float
+                      help="Ratio of noise")
+    parser.add_option("--tolerance", "-i", dest="tolerance", default=1, type=int
+                      help="Threshold")
+
+    return parser
+
+
+
+if __name__ == "__main__":
+    if not os.path.isdir("out"):
+        print("Directory for outputs, 'out' not present, creating it.")
+        os.mkdir("out")
+
+    # parse opts
+    parser = set_up_parser()
+    (opts, args) = parser.parse_args()
+
+    print("c Seed: ", opts.seed)
+    print("c N: ", opts.n)
+    print("c Samples: ", opts.samples)
+    print("c Noise: ", opts.noise)
+    print("c Tolerance: ", opts.tolerance)
+    random.seed(opts.seed)
+
+    # y[i] = set randomly [[output]]
+    # x[i] = data that SAT solver can manipulate
+    # val[x][y] = x[i]*y[i]
+
+
+    fun = []
+    for i in range(opts.n):
+        fun.append(random.randint(1, 0))
+
+    inputs = []
+    for i in range(opts.samples):
+        inp = []
+        for i2 in range(opts.n):
+            inp.append(random.randint(1, 0))
+        inputs.append(inp)
+
+    real_outputs = []
+    for i in range(opts.samples):
+        out = 0
+        for a,b in zip(fun, inputs[i]):
+            out ^= fun*inputs[i]
+        real_outputs.append(out)
+
+    num_incorrect_eqs = 0
+    correct_eqs = []
+    outputs = []
+    for i in range(opts.samples):
+        out = real_outputs[i]
+        if random.random() < opts.noise:
+             out = out ^ 1
+             incorrect_eqs+=1
+             correct_eqs.append(False)
+         else:
+             correct_eqs.append(True)
+        outputs.append(out)
+
+    # print
+    for i in range(opts.samples):
+        toprint = ""
+        for i2 in range(opts.n):
+            toprint += "%d" % fun[i2]
+            toprint += "*"
+            toprint += "%d" % inputs[i][i2]
+            if i2 != opts.n-1:
+                toprint += " + "
+        toprint += " %d" % outputs[i]
+        toprint += "  -- correct: %s" % correct_eqs[i]
+        print(toprint)
+
+
+
+
+
+
+
+
+
