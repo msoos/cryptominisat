a68a7abe0d0ecd0507f27ec144237ae39da99883
Mate Soos <soos.mate@gmail.com>
2022-11-04 01:22:35 +0100

    Fixing appveyor script

--- scripts/appveyor.ps1
+++ scripts/appveyor.ps1
@@ -10,4 +10,4 @@ function Unzip
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile("https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.zip", "C:\projects\cryptominisat\boost_1_80_0.zip")
 
-Unzip "C:\projects\cryptominisat\boost_1_59_0.zip" "C:\projects\cryptominisat"
+Unzip "C:\projects\cryptominisat\boost_1_80_0.zip" "C:\projects\cryptominisat"
