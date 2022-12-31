267cc385bc73443f024bbce0edba9c343c6b4f9f
Martin Maurer <referme@gmx.de>
2022-12-31 11:51:05 +0100

    Update main_exe.cpp
    Catch boost errors to avoid core dump in case of wrong command line parameters

--- src/main_exe.cpp
+++ src/main_exe.cpp
@@ -50,6 +50,9 @@ int main(int argc, char** argv)
     } catch (CMSat::TooLongClauseError& e) {
         std::cerr << "ERROR! Too long clause inserted" << std::endl;
         exit(-1);
+    } catch (const boost::program_options::error& ex) {
+        std::cerr << "Error: " << ex.what() << std::endl;
+        exit(-1);
     }
 
     return ret;
