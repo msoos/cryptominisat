6d9a90eeb05b1051b1f8a8b29c1be73e0f652cbe
Mate Soos <soos.mate@gmail.com>
2023-10-14 19:11:01 +0200

    No more version in windows python module

--- python/src/pycryptosat.cpp
+++ python/src/pycryptosat.cpp
@@ -897,14 +897,6 @@ MODULE_INIT_FUNC(pycryptosat)
     // Add the version string so users know what version of CryptoMiniSat
     // they're using.
 #if defined(_MSC_VER)
-    if (PyModule_AddStringConstant(m, "__version__", "unknown -- can't use /D to define strings in Windows, pypi and Windows CLI are both broken") == -1 {
-        Py_DECREF(m);
-        return NULL;
-    }
-    if (PyModule_AddStringConstant(m, "VERSION", "unknown -- can't use /D to define strings in Windows, pypi and Windows CLI are both broken") == -1) {
-        Py_DECREF(m);
-        return NULL;
-    }
 #else
     if (PyModule_AddStringConstant(m, "__version__", CMS_FULL_VERSION) == -1) {
         Py_DECREF(m);
