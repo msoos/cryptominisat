a319494ca920f7612d12dc4a09fd72052e3c6219
Mate Soos <soos.mate@gmail.com>
2024-01-11 20:34:45 +0100

    Better checking

--- scripts/final_check.py
+++ scripts/final_check.py
@@ -7,8 +7,8 @@ if len(sys.argv) != 2:
     print("Must give proof")
     exit(-1)
 
-cls = {}
-xls = {}
+cls_store = {}
+xls_store = {}
 line_no = 0
 with open(sys.argv[1], "r") as f:
     for line in f:
@@ -17,31 +17,73 @@ with open(sys.argv[1], "r") as f:
         if len(line) == 0:
             print("EMTPY LINE!!")
             exit(-1)
-        # imply cl from x
+
+        x = None
+        cl = None
+        xid = None
+        xls = None
+        cls = None
+        cl_xs = None
+        xs = None
         if line[0] == 'i':
-            cl_xs = line[1:].split('l')
-            assert len(cl_xs) == 2
+            assert line[1] == ' '
+            assert len(line) > 2
+            if line[2] == 'x':
+                # imply x from cls
+                cl_xs = line[3:].split('l')
+                assert len(cl_xs) == 2
 
-            cl = cl_xs[0].split()
-            id = int(cl[0])
-            cl = cl[1:]
-            if id in cls:
-                print("ERROR, implied cl ", id, " already in cls")
-                print("line no: ", line_no)
-                print("line: ", line)
-                exit(-1)
-            cl = [int(l) for l in cl]
-            cls[id] = sorted(cl)
-
-            xs = cl_xs[1].split()
-            xs = xs[:-1]
-            for x in xs:
-                if int(x) not in xls:
-                    print("ERROR, cl implied by x ", x , " not in xls")
+                x = cl_xs[0].split()
+                xid = int(x[0])
+                x = x[1:]
+                if xid in xls_store:
+                    print("ERROR, implied x ", xid, " already in cls. lits:", x)
                     print("line no: ", line_no)
                     print("line: ", line)
                     exit(-1)
-                assert x in xls
+                x = [int(l) for l in x]
+                assert xid not in xls_store
+                xls_store[xid] = sorted(x)
+
+                cls = cl_xs[1].split()
+                cls = cls[:-1]
+                cls = [int(id) for id in cls]
+                for id in cls_store:
+                    # assert all the same length!
+                    if id not in cls_store:
+                        print("ERROR, clause used to imply ", xid, " id: " id, " not in xls_store")
+                        print("line no: ", line_no)
+                        print("line: ", line)
+                        exit(-1)
+                    assert id in cls_store
+                pass
+            else:
+                # imply cl from x
+                cl_xs = line[1:].split('l')
+                assert len(cl_xs) == 2
+
+                cl = cl_xs[0].split()
+                id = int(cl[0])
+                cl = cl[1:]
+                if id in cls_store:
+                    print("ERROR, implied cl ", id, " already in cls_store")
+                    print("line no: ", line_no)
+                    print("line: ", line)
+                    exit(-1)
+                cl = [int(l) for l in cl]
+                assert id not in cls_store
+                cls_store[id] = sorted(cl)
+
+                xs = cl_xs[1].split()
+                xs = xs[:-1]
+                assert len(xs) == 1
+                xid = int(xs[0])
+                if xid not in xls_store:
+                    print("ERROR, cl implied by x ", xid , " not in xls_store")
+                    print("line no: ", line_no)
+                    print("line: ", line)
+                    exit(-1)
+                assert xid in xls_store
             continue
 
         if line[0] == 'a' or line[0] == 'o':
@@ -51,26 +93,27 @@ with open(sys.argv[1], "r") as f:
             assert len(x_or_cl) >= 2
             if x_or_cl[0].strip() == 'x':
                 x = line[1:].split()
-                id = int(x[0])
+                xid = int(x[0])
                 ls = [int(l) for l in x[1:]]
-                if id in xls:
-                    print("ERROR, added/orig x ", id, " already in xls")
+                if xid in xls_store:
+                    print("ERROR, added/orig x ", id, " already in xls_store")
                     print("line no: ", line_no)
                     print("line: ", line)
                     exit(-1)
-                assert id not in xls
-                xls[id] = sorted(ls)
+                assert xid not in xls_store
+                xls_store[xid] = sorted(ls)
             else:
                 c = str(line).split()
                 id = int(c[0])
                 ls = [int(l) for l in c[1:]]
-                if id in cls:
-                    print("ERROR, added/orig CL ", id, " already in cls")
+                if id in cls_store:
+                    print("ERROR, added/orig CL ", id, " already in cls_store")
                     print("line no: ", line_no)
                     print("line: ", line)
                     exit(-1)
-                assert id not in xls
-                xls[id] = sorted(ls)
+                assert id not in xls_store
+                xls_store[id] = sorted(ls)
+            continue
 
         if line[0] == 'd' or line[0] == 'f':
             assert line[1] == ' '
@@ -81,28 +124,34 @@ with open(sys.argv[1], "r") as f:
                 x = line[1:].split()
                 id = int(x[0])
                 ls = [int(l) for l in x[1:]]
-                if id not in xls:
-                    print("ERROR, deleted/finalized x ", id, " not in xls")
+                if id not in xls_store:
+                    print("ERROR, deleted/finalized x ", id, " not in xls_store")
                     print("line no: ", line_no)
                     print("line: ", line)
                     exit(-1)
-                assert id in xls
-                xls.pop(id)
+                assert id in xls_store
+                xls_store.pop(id)
             else:
                 c = str(line).split()
                 id = int(c[0])
                 ls = [int(l) for l in c[1:]]
-                if id in cls:
-                    print("ERROR, deleted/finalized CL ", id, " not in cls")
+                if id in cls_store:
+                    print("ERROR, deleted/finalized CL ", id, " not in cls_store")
                     print("line no: ", line_no)
                     print("line: ", line)
                     exit(-1)
-                assert id in xls
-                xls.pop(id)
+                assert id in cls_store
+                cls_store.pop(id)
+            continue
 
-for id, lits in xls.items():
+        if line[0] == 'c':
+            continue
+
+        print("cannot understand: ", line)
+
+for id, lits in xls_store.items():
     print("ERROR, x ID: ", id, " not finalized. lits: ", lits)
 
-for id, lits in cls.items():
+for id, lits in cls_store.items():
     print("ERROR, cl ID: ", id, " not finalized. lits: ", lits)
 
