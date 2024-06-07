4fef50c48f845a86f95c8abd65d2ccba4f471393
Mate Soos <soos.mate@gmail.com>
2024-06-07 15:33:49 -0400

    Revert "Allow old backbone to be called"
    This reverts commit d43d56a5ec790219d11ab0305546b79e0df4d817.

--- src/cryptominisat.cpp
+++ src/cryptominisat.cpp
@@ -1830,11 +1830,11 @@ DLL_PUBLIC bool SATSolver::minimize_clause(std::vector<Lit>& cl)
 }
 
 
-DLL_PUBLIC bool SATSolver::backbone_simpl(int64_t max_confl, double maxtime, bool& finished)
+DLL_PUBLIC bool SATSolver::backbone_simpl(int64_t max_confl, bool& finished)
 {
     Solver& s = *data->solvers[0];
     actually_add_clauses_to_threads(data);
-    return s.backbone_simpl(max_confl, true, maxtime, finished);
+    return s.backbone_simpl(max_confl, true, finished);
 }
 
 DLL_PUBLIC bool SATSolver::removed_var(uint32_t var) const{
