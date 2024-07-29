b72f87cebdb349385fe3f83c8cba69cfc8f92132
Mate Soos <soos.mate@gmail.com>
2024-07-29 20:33:15 +0200

    Fixing up this, we can just mark it as removed

--- src/occsimplifier.cpp
+++ src/occsimplifier.cpp
@@ -3285,7 +3285,7 @@ void OccSimplifier::rem_cls_from_watch_due_to_varelim(const Lit lit , bool add_t
 
             //Remove -- only FRAT the ones that are redundant
             //The irred will be removed thanks to 'elimed' system
-            unlink_clause(offset, cl.red(), true, false);
+            unlink_clause(offset, cl.red(), true, true);
         } else if (watch.isBin()) {
             //Update stats
             if (!watch.red()) {
