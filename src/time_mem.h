7db297401542981336eea80fac8b408cdcc744da
Mate Soos <soos.mate@gmail.com>
2026-03-06 21:58:42 +0100

    Fixing emscripten build

--- src/time_mem.h
+++ src/time_mem.h
@@ -33,7 +33,7 @@ THE SOFTWARE.
 #include <cstdint>
 
 // note: MinGW64 defines both __MINGW32__ and __MINGW64__
-#if defined (_MSC_VER) || defined (__MINGW32__) || defined(_WIN32) || defined(__EMSCRIPTEN__)
+#if defined (_MSC_VER) || defined (__MINGW32__) || defined(_WIN32)
 #include <ctime>
 #include <windows.h>
 static inline double cpu_time(void)
