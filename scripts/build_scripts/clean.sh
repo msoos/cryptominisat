24f36ef8399abf60b4a7c628beb666d1d10812c9
Mate Soos <soos.mate@gmail.com>
2026-04-03 13:01:39 +0200

    Add clean
    Update mode

---
+++ scripts/build_scripts/clean.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+rm -rf .cmake
+rm -rf lib*
+rm -rf Test*
+rm -rf tests*
+rm -rf include
+rm -rf tests
+rm -rf CM*
+rm -rf cmake*
+rm -rf arjun
+rm -rf Makefile
+rm -rf rjun-src
+rm -rf deps
+rm -rf _deps
