0faf66e80b14f587f035d938cfc328430c32ca6b
Mate Soos <soos.mate@gmail.com>
2026-09-01 00:04:53 +0200

    Don't turn of SLS here, it now works for XORs, too

--- src/cryptominisat.cpp
+++ src/cryptominisat.cpp
@@ -1415,7 +1415,6 @@ void DLL_PUBLIC SATSolver::set_up_for_scalmc()
         conf.global_multiplier_multiplier_max = 1;
         conf.orig_global_timeout_multiplier = 1.5;
         conf.min_bva_gain = 1;
-        conf.doSLS = false;
         conf.xor_finder_time_limitM = 400;
 //         conf.polar_stable_every_n = 100000; //i.e. never
         uint32_t xor_cut = 4;
